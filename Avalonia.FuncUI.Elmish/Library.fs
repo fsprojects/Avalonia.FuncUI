e4b5048444ec1a86461f0b4d65270dfe7b69a8ad
JaggerJo <jaeger.josua@gmail.com>
2019-05-23 00:21:24 +0200

    Elmish

---
+++ Avalonia.FuncUI.Elmish/Library.fs
@@ -0,0 +1,14 @@
+﻿namespace Avalonia.FuncUI.Elmish
+
+open Avalonia.FuncUI.Hosts
+open Elmish
+open Avalonia.FuncUI.Core
+
+module Program =
+
+    let withHost (host: IViewHost) (program: Program<'arg, 'model, 'msg, ViewElement>) =
+        let setState state dispatch =
+            let view = ((Program.view program) state dispatch)
+            host.View view
+
+        program |> Program.withSetState setState
