7ced32d34e3a4af83a0d9a8b68ab2502d3fad294
JaggerJo <jaeger.josua@gmail.com>
2019-05-25 11:11:20 +0200

    add Command Helpers

---
+++ Avalonia.FuncUI/Helpers.fs
@@ -0,0 +1,47 @@
+﻿namespace Avalonia.FuncUI.Builders
+
+module Command =
+    open System.Windows.Input
+
+    (* canExecuteChanged is not implemented correctly, only to satisfy the interface *)
+    type private FunCommand =   
+        val private canExecuteChanged: Event<System.EventHandler, System.EventArgs>
+        val private execute: obj -> unit
+        val private canExecute: obj -> bool
+        
+        new (execute) =
+            {
+                canExecute = fun param -> true
+                execute = execute
+                canExecuteChanged = new Event<_, _>()
+            }
+
+        new (execute, canExecute) =
+            {
+                canExecute = fun param -> canExecute
+                execute = execute
+                canExecuteChanged = new Event<_, _>()
+            }
+
+        new (execute, canExecute) =
+            {
+                canExecute = canExecute
+                execute = execute
+                canExecuteChanged = new Event<_, _>()
+            }
+
+
+        interface ICommand with
+
+            [<CLIEvent>]
+            member this.CanExecuteChanged = this.canExecuteChanged.Publish
+                
+            member this.Execute (param: obj) =
+                this.execute param
+
+            member this.CanExecute (param: obj) =
+                this.canExecute param
+
+    let from (execute: obj -> unit) : ICommand =
+        FunCommand(execute) :> ICommand
+
