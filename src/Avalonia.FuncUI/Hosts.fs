9c89bbdbf1428d25e12b3717dfa57038de932464
JaggerJo <jaeger.josua@gmail.com>
2019-06-21 19:01:44 +0200

    add 'HostControl' to enable embedding into existing apps

--- src/Avalonia.FuncUI/Hosts.fs
+++ src/Avalonia.FuncUI/Hosts.fs
@@ -27,5 +27,25 @@ type HostWindow() =
                 VirtualDom.Patcher.patch(view, delta)
 
             lastViewElement <- Some viewElement
+
+type HostControl() =
+    inherit ContentControl()
+
+    let mutable lastViewElement : View option = None
+
+    interface IViewHost with
+        member this.UpdateView viewElement =
+            match lastViewElement with
+            | Some last ->
+                let view = this.Content :?> IControl
+                let delta = VirtualDom.Differ.diff(last, viewElement)
+                VirtualDom.Patcher.patch(view, delta)
+            | None ->
+                this.Content <- VirtualDom.createView viewElement
+                let view = this.Content :?> IControl
+                let delta = ViewDelta.From viewElement
+                VirtualDom.Patcher.patch(view, delta)
+
+            lastViewElement <- Some viewElement
        
     
