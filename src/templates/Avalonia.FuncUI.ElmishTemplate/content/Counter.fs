4247344caa6e20ad809fe47a67d34d60d094a4d0
Kevin Schneider <kevin-schneider@mutagene.de>
2019-07-10 15:37:46 +0200

    Add dotnet template folder and template project for ElmishTemplate

---
+++ src/templates/Avalonia.FuncUI.ElmishTemplate/content/Counter.fs
@@ -0,0 +1,60 @@
+﻿namespace CounterElmishSample
+
+open Avalonia.Controls
+open Avalonia.Media
+open Avalonia.FuncUI.Types
+open Avalonia.FuncUI
+open Avalonia.Layout
+
+type CustomControl() =
+    inherit Control()
+
+    member val Text: string = "" with get, set
+
+[<AutoOpen>]
+module ViewExt =
+    type Views with
+        static member customControl (attrs: TypedAttr<CustomControl> list): View =
+            Views.create<CustomControl>(attrs)
+
+module Counter =
+
+    type CounterState = {
+        count : int
+    }
+
+    let init = {
+        count = 0
+    }
+
+    type Msg =
+    | Increment
+    | Decrement
+
+    let update (msg: Msg) (state: CounterState) : CounterState =
+        match msg with
+        | Increment -> { state with count =  state.count + 1 }
+        | Decrement -> { state with count =  state.count - 1 }
+    
+    let view (state: CounterState) (dispatch): View =
+        Views.dockpanel [
+            Attrs.children [
+                Views.button [
+                    Attrs.dockPanel_dock Dock.Bottom
+                    Attrs.onClick (fun sender args -> dispatch Decrement)
+                    Attrs.content "-"
+                ]
+                Views.button [
+                    Attrs.dockPanel_dock Dock.Bottom
+                    Attrs.onClick (fun sender args -> dispatch Increment)
+                    Attrs.content "+"
+                ]
+                Views.textBlock [
+                    Attrs.dockPanel_dock Dock.Top
+                    Attrs.fontSize 48.0
+                    Attrs.verticalAlignment VerticalAlignment.Center
+                    Attrs.horizontalAlignment HorizontalAlignment.Center
+                    Attrs.text (string state.count)
+                ]
+            ]
+        ]       
---
+++ src/templates/Avalonia.FuncUI.ElmishTemplate/content/Lib.fs
@@ -0,0 +1,13 @@
+﻿namespace CounterElmishSample
+
+[<AutoOpen>]
+module AvaloniaExtensions =
+    open Avalonia.Markup.Xaml.Styling
+    open System
+    open Avalonia.Styling
+
+    type Styles with
+        member this.Load (source: string) = 
+            let style = new StyleInclude(baseUri = null)
+            style.Source <- new Uri(source)
+            this.Add(style)
