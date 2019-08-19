67df220eeb83ec25b962064de477d8478043046b
Josua Jäger <jaeger.josua@gmail.com>
2019-08-19 23:02:24 +0200

    wip

--- src/Avalonia.FuncUI/Core.fs
+++ src/Avalonia.FuncUI/Core.fs
@@ -1,8 +1,10 @@
 ﻿namespace Avalonia.FuncUI
 
+open System
+
 module Core =
     
-    module Domain =
+    module rec Domain =
        
         type Accessor =
             | Instance of string
@@ -11,15 +13,55 @@ module Core =
         type Property =
             {
                 accessor: Accessor
-                
+                value: obj
             }
         
         type Content =
             {
                 accessor: Accessor
-            }
+            }   
+            
+        type IAttr =
+            interface end
+              
+        type IAttr<'viewType> =
+            inherit IAttr
         
-        type Attr =
-            | Content
-            | Property
-            | Event
+        type IView =
+            abstract member ViewType: Type with get
+            abstract member Attrs: IAttr list with get
+            
+        type IView<'viewType> =
+            inherit IView
+            abstract member Attrs: IAttr<'viewType> list with get
+            
+        type View<'viewType> =
+            {
+                viewType: Type
+                attrs: IAttr<'viewType> list
+            }
+            
+            interface IView with
+                member this.ViewType =  this.viewType
+                member this.Attrs = this.attrs |> List.map (fun attr -> attr :> IAttr)
+                
+            interface IView<'viewType> with
+                member this.Attrs = this.attrs
+                
+        module View =
+            
+            /// create a new strongly typed View
+            let create<'viewType>(attrs: #IAttr<'viewType> list) : IView<'viewType> =
+                {
+                    View.viewType = typeof<'viewType>
+                    View.attrs = attrs
+                }
+                :> IView<'viewType>
+                
+            /// create a new loosely typed View
+            let create'<'viewType>(attrs: #IAttr<'viewType> list) : IView =
+                {
+                    View.viewType = typeof<'viewType>
+                    View.attrs = attrs
+                }
+                :> IView
