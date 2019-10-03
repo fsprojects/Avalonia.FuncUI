b63278a80ada93bb447e447691cac2330e88be2a
Don Syme <dsyme@microsoft.com>
2019-10-03 16:30:10 +0100

    adaptive micro sample for funcui

--- src/Avalonia.FuncUI/Core/Types.fs
+++ src/Avalonia.FuncUI/Core/Types.fs
@@ -1,6 +1,7 @@
 ﻿namespace Avalonia.FuncUI
 
 open System
+open FSharp.Data.Adaptive
 
 module rec Types =
 
@@ -10,6 +11,12 @@ module rec Types =
             Value : obj
         }
 
+    type AdaptivePropertyAttr =
+        {
+            Name : string
+            Value : aval<obj>
+        }
+
     [<CustomEquality; NoComparison>]
     type AttachedPropertyAttr =
         {
@@ -17,12 +24,11 @@ module rec Types =
             Value : obj
             Handler : obj * (obj option) -> unit
         }
-        with 
-            override this.GetHashCode() = 
-                (this.Name, this.Value).GetHashCode()
+        override this.GetHashCode() = 
+            (this.Name, this.Value).GetHashCode()
 
-            override this.Equals other =
-                this.GetHashCode() = other.GetHashCode()
+        override this.Equals other =
+            this.GetHashCode() = other.GetHashCode()
 
     [<CustomEquality; NoComparison>]
     type EventAttr =
@@ -30,13 +36,12 @@ module rec Types =
             Name : string
             Value : Delegate
         }
-        with 
-            override this.GetHashCode() = 
-                this.Name.GetHashCode()
+        override this.GetHashCode() = 
+            this.Name.GetHashCode()
 
-            override this.Equals other =
-                // TODO: find a better way to do this
-                false // always set event
+        override this.Equals other =
+            // TODO: find a better way to do this
+            false // always set event
 
     type ContentAttr =
         {
@@ -44,34 +49,60 @@ module rec Types =
             Content : ViewContent
         }
 
+    type AdaptiveContentAttr =
+        {
+            Name : string
+            Content : AdaptiveViewContent
+        }
+
     type ViewContent = 
         | Single of View option
         | Multiple of View list
 
+    type AdaptiveViewContent = 
+        | Single of AdaptiveView option
+        | Multiple of alist<AdaptiveView>
+
     type Lifecycle = OnCreate | OnUpdate
 
     [<CustomEquality; NoComparison>]
-    type LifecylceAttr =
+    type LifecycleAttr =
         {
             Lifecylce : Lifecycle
             Func : obj -> unit
         }
-        with 
-            override this.GetHashCode() = 
-                this.Lifecylce.GetHashCode()
+        override this.GetHashCode() = 
+            this.Lifecylce.GetHashCode()
 
-            override this.Equals other =
-                this.GetHashCode() = other.GetHashCode()
+        override this.Equals other =
+            this.GetHashCode() = other.GetHashCode()
+
+    type AdaptiveLifecylceAttr = LifecycleAttr
             
     type Attr =
         | Property of PropertyAttr
         | AttachedProperty of AttachedPropertyAttr
         | Event of EventAttr
         | Content of ContentAttr
-        | Lifecycle of LifecylceAttr
+        | Lifecycle of LifecycleAttr
+
+    [<RequireQualifiedAccess>]
+    type AdaptiveAttr =
+        | Property of AdaptivePropertyAttr
+        | AttachedProperty of AttachedPropertyAttr
+        | Event of EventAttr
+        | Content of AdaptiveContentAttr
+        | Lifecycle of AdaptiveLifecylceAttr
 
     type View =
         {
             ViewType: Type
             Attrs: Attr list
+        }  
+
+    [<RequireQualifiedAccess>]
+    type AdaptiveView =
+        {
+            ViewType: aval<Type>
+            Attrs: alist<Attr>
         }  
