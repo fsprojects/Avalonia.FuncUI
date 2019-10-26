f9fb9bec67b2ecf3232491fa785b966c262549c5
Josua Jäger <jaeger.josua@gmail.com>
2019-10-26 18:20:00 +0200

    add DSL for UniformGrid

---
+++ src/Avalonia.FuncUI.DSL/Panels/UniformGrid.fs
@@ -0,0 +1,20 @@
+﻿namespace Avalonia.FuncUI.DSL
+
+[<AutoOpen>]
+module UniformGrid =
+    open Avalonia.Controls.Primitives
+    open Avalonia.FuncUI.Types
+    open Avalonia.FuncUI.Builder
+   
+    let create (attrs: IAttr<UniformGrid> list): IView<UniformGrid> =
+        ViewBuilder.Create<UniformGrid>(attrs)
+
+    type UniformGrid with
+        static member rows<'t when 't :> UniformGrid>(count: int) : IAttr<'t> =
+            AttrBuilder<'t>.CreateProperty<int>(UniformGrid.RowsProperty, count, ValueNone)
+            
+        static member columns<'t when 't :> UniformGrid>(count: int) : IAttr<'t> =
+            AttrBuilder<'t>.CreateProperty<int>(UniformGrid.ColumnsProperty, count, ValueNone)
+            
+        static member firstColumn<'t when 't :> UniformGrid>(column: int) : IAttr<'t> =
+            AttrBuilder<'t>.CreateProperty<int>(UniformGrid.FirstColumnProperty, column, ValueNone)
