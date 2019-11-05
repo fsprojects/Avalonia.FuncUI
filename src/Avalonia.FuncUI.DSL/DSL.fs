60aeeb603ce31110d958585a6061af399d2c9722
Josua Jäger <jaeger.josua@gmail.com>
2019-11-05 21:46:09 +0100

    wip

--- src/Avalonia.FuncUI.DSL/DSL.fs
+++ src/Avalonia.FuncUI.DSL/DSL.fs
@@ -1,9 +1,21 @@
 namespace Avalonia.FuncUI.DSL
 
-open Avalonia.FuncUI.Types
-open System.Threading
-
 [<AutoOpen>]
 module Helpers =
+    open Avalonia.FuncUI.Types
+    
     let generalize (view: IView<'t>) : IView =
         view :> IView
+        
+        
+[<AutoOpen>]
+module AvaloniaExtensions =
+    open Avalonia.Markup.Xaml.Styling
+    open System
+    open Avalonia.Styling
+
+    type Styles with
+        member this.Load (source: string) = 
+            let style = new StyleInclude(baseUri = null)
+            style.Source <- new Uri(source)
+            this.Add(style)
---
+++ src/Examples/Examples.CounterApp/Program.fs
@@ -0,0 +1,46 @@
+﻿namespace Examples.CounterApp
+
+open Avalonia
+open Avalonia.FuncUI.Components.Hosts
+open Avalonia.FuncUI.DSL
+open Avalonia.FuncUI.Elmish
+open Avalonia.Controls.ApplicationLifetimes
+open Elmish
+
+type MainWindow() as this =
+    inherit HostWindow()
+    do
+        base.Title <- "Counter Example"
+        base.Height <- 400.0
+        base.Width <- 400.0    
+      
+        //this.VisualRoot.VisualRoot.Renderer.DrawFps <- true
+        //this.VisualRoot.VisualRoot.Renderer.DrawDirtyRects <- true
+        Elmish.Program.mkSimple (fun () -> Counter.init) Counter.update Counter.view
+        |> Program.withHost this
+        |> Program.withConsoleTrace
+        |> Program.run
+        
+type App() =
+    inherit Application()
+
+    override this.Initialize() =
+        this.Styles.Load "resm:Avalonia.Themes.Default.DefaultTheme.xaml?assembly=Avalonia.Themes.Default"
+        this.Styles.Load "resm:Avalonia.Themes.Default.Accents.BaseDark.xaml?assembly=Avalonia.Themes.Default"
+
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
+            let mainWindow = MainWindow()
+            desktopLifetime.MainWindow <- mainWindow
+        | _ -> ()
+
+module Program =
+
+    [<EntryPoint>]
+    let main(args: string[]) =
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .UseSkia()
+            .StartWithClassicDesktopLifetime(args)
