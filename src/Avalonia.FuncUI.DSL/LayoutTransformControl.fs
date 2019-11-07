30323e7f6d397de4fe2787c27ecfad4ef8e59da0
Josua Jäger <jaeger.josua@gmail.com>
2019-11-07 21:43:44 +0100

    wip

---
+++ src/Avalonia.FuncUI.DSL/LayoutTransformControl.fs
@@ -0,0 +1,18 @@
+﻿namespace Avalonia.FuncUI.DSL
+
+[<AutoOpen>]
+module LayoutTransformControl =
+    open Avalonia.Media
+    open Avalonia.Controls
+    open Avalonia.FuncUI.Types
+    open Avalonia.FuncUI.Builder
+   
+    let create (attrs: IAttr<LayoutTransformControl> list): IView<LayoutTransformControl> =
+        ViewBuilder.Create<LayoutTransformControl>(attrs)
+
+    type LayoutTransformControl with
+        static member layoutTransform<'t when 't :> LayoutTransformControl>(value: Transform) : IAttr<'t> =
+            AttrBuilder<'t>.CreateProperty<Transform>(LayoutTransformControl.TransitionsProperty, value, ValueNone)
+            
+        static member useRenderTransform<'t when 't :> LayoutTransformControl>(value: bool) : IAttr<'t> =
+            AttrBuilder<'t>.CreateProperty<bool>(LayoutTransformControl.UseRenderTransformProperty, value, ValueNone)
