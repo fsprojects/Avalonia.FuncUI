52cef905f3431ed1cf585e4733b35d00bc01521e
Josua Jäger <jaeger.josua@gmail.com>
2019-11-13 21:54:45 +0100

    add Clock example (#44)

---
+++ src/Examples/Examples.ClockApp/Clock.fs
@@ -0,0 +1,83 @@
+namespace Examples.ClockApp
+open Avalonia
+open Avalonia.FuncUI.DSL
+open Avalonia.FuncUI.DSL
+
+module Clock =
+    open System
+    open Avalonia.Controls
+    open Avalonia.Controls.Shapes
+    
+    type State = { time : DateTime }
+    let init = { time = DateTime.Now }
+
+    type Msg = Tick of DateTime
+
+    let update (msg: Msg) (state: State) : State =
+        match msg with
+        | Tick time -> { state with time = time }
+    
+    type PointerType = Hour | Minute | Second
+    
+    let calcPointerPosition (pointer: PointerType, time: DateTime) : Point =
+        let percent =
+            match pointer with
+            | Hour -> (float time.Hour) / 12.0
+            | Minute -> (float time.Minute) / 60.0
+            | Second -> (float time.Second) / 60.0
+            
+        let length =
+            match pointer with
+            | Hour -> 50.0
+            | Minute -> 60.0
+            | Second -> 70.0
+            
+        let angle = 2.0 * Math.PI * percent
+        let handX = (100.0 + length * cos (angle - Math.PI / 2.0))
+        let handY = (100.0 + length * sin (angle - Math.PI / 2.0))
+        Point(handX, handY)
+        
+    
+    let view (state: State) (dispatch) =
+        Canvas.create [
+            Canvas.background "#2c3e50"
+            Canvas.children [
+                
+                Ellipse.create [
+                    Ellipse.top 10.0
+                    Ellipse.left 10.0
+                    Ellipse.width 180.0
+                    Ellipse.height 180.0
+                    Ellipse.fill "#ecf0f1"
+                ]
+                
+                Line.create [
+                    Line.startPoint (100.0, 100.0)
+                    Line.endPoint (calcPointerPosition(Second, state.time))
+                    Line.strokeThickness 2.0
+                    Line.stroke "#e74c3c"
+                ]
+                
+                Line.create [
+                    Line.startPoint (100.0, 100.0)
+                    Line.endPoint (calcPointerPosition(Minute, state.time))
+                    Line.strokeThickness 4.0
+                    Line.stroke "#7f8c8d"
+                ]
+                
+                Line.create [
+                    Line.startPoint (100.0, 100.0)
+                    Line.endPoint (calcPointerPosition(Hour, state.time))
+                    Line.strokeThickness 6.0
+                    Line.stroke "black"
+                ]
+                
+                Ellipse.create [
+                    Ellipse.top 95.0
+                    Ellipse.left 95.0
+                    Ellipse.width 10.0
+                    Ellipse.height 10.0
+                    Ellipse.fill "#95a5a6"
+                ]
+            ]
+        ]       
---
+++ src/Examples/Examples.ClockApp/Program.fs
@@ -0,0 +1,65 @@
+﻿namespace Examples.ClockApp
+
+open System
+open Avalonia
+open Avalonia.Threading
+open Elmish
+open Avalonia.FuncUI.Components.Hosts
+open Avalonia.FuncUI
+open Avalonia.FuncUI.Elmish
+open Avalonia.Controls.ApplicationLifetimes
+
+type MainWindow() as this =
+    inherit HostWindow()
+    do
+        base.Title <- "Clock Example"
+        
+        base.Height <- 200.0
+        base.MaxHeight <- 200.0
+        base.MinHeight <- 200.0
+        
+        base.Width <- 200.0
+        base.MaxWidth <- 200.0
+        base.MinWidth <- 200.0
+      
+        let timer (state: Clock.State) =
+            let sub (dispatch: Clock.Msg -> unit) =
+                let invoke() =
+                    DateTime.Now |> Clock.Msg.Tick |> dispatch
+                    true
+                    
+                DispatcherTimer.Run(Func<bool>(invoke), TimeSpan.FromMilliseconds 1000.0) |> ignore
+                
+            Cmd.ofSub sub
+        
+        //this.VisualRoot.VisualRoot.Renderer.DrawFps <- true
+        //this.VisualRoot.VisualRoot.Renderer.DrawDirtyRects <- true
+        Elmish.Program.mkSimple (fun () -> Clock.init) Clock.update Clock.view
+        |> Program.withHost this
+        |> Program.withSubscription timer
+        |> Program.withConsoleTrace
+        |> Program.run
+        
+type App() =
+    inherit Application()
+
+    override this.Initialize() =
+        this.Styles.Load "resm:Avalonia.Themes.Default.DefaultTheme.xaml?assembly=Avalonia.Themes.Default"
+        this.Styles.Load "resm:Avalonia.Themes.Default.Accents.BaseDark.xaml?assembly=Avalonia.Themes.Default"
+
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
+            let mainWindow = MainWindow()
+            desktopLifetime.MainWindow <- mainWindow
+        | _ -> ()
+
+module Program =
+
+    [<EntryPoint>]
+    let main(args: string[]) =
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .UseSkia()
+            .StartWithClassicDesktopLifetime(args)
