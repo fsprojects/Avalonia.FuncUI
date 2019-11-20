2b9f4e558d357112fe35d301afb353295f737eab
Josua Jäger <jaeger.josua@gmail.com>
2019-11-20 22:32:37 +0100

    improve and add bindings

--- src/Avalonia.FuncUI.DSL/ToggleButton.fs
+++ src/Avalonia.FuncUI.DSL/ToggleButton.fs
@@ -2,6 +2,7 @@ namespace Avalonia.FuncUI.DSL
 
 [<AutoOpen>]
 module ToggleButton =
+    open System
     open Avalonia.Controls.Primitives
     open Avalonia.FuncUI.Types
     open Avalonia.FuncUI.Builder
@@ -13,8 +14,14 @@ module ToggleButton =
         static member isThreeState<'t when 't :> ToggleButton>(value: bool) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<bool>(ToggleButton.IsThreeStateProperty, value, ValueNone)
             
+        static member isChecked<'t when 't :> ToggleButton>(value: Nullable<bool>) : IAttr<'t> =
+            AttrBuilder<'t>.CreateProperty<Nullable<bool>>(ToggleButton.IsCheckedProperty, value, ValueNone)
+            
         static member isChecked<'t when 't :> ToggleButton>(value: bool) : IAttr<'t> =
-            AttrBuilder<'t>.CreateProperty<bool>(ToggleButton.IsCheckedProperty, value, ValueNone)
-
-       
-
+            value |> Nullable |> ToggleButton.isChecked
+            
+        static member isChecked<'t when 't :> ToggleButton>(value: bool option) : IAttr<'t> =
+            value |> Option.toNullable |> ToggleButton.isChecked
+            
+        static member onIsCheckedChanged<'t when 't :> ToggleButton>(func: Nullable<bool> -> unit) : IAttr<'t> =
+            AttrBuilder<'t>.CreateSubscription<Nullable<bool>>(ToggleButton.IsCheckedProperty, func)
