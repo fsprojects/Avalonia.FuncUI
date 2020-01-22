dda0e7afd4b02eaf8b4ef3c642abac72eac59184
Angel D. Munoz <scythes.lol@gmail.com>
2020-01-21 18:44:39 -0700

    add calendar item

---
+++ src/Avalonia.FuncUI.DSL/Calendar/CalendarItem.fs
@@ -0,0 +1,15 @@
+﻿namespace Avalonia.FuncUI.DSL.Primitives
+
+
+[<AutoOpen>]
+module CalendarItem =
+    open Avalonia.Controls.Primitives
+    open Avalonia.Media
+    open Avalonia.FuncUI.Types
+    open Avalonia.FuncUI.Builder
+    
+    let create (attrs: IAttr<CalendarItem> list): IView<CalendarItem> =
+        ViewBuilder.Create<CalendarItem>(attrs)
+     
+    type CalendarItem with
+        end
