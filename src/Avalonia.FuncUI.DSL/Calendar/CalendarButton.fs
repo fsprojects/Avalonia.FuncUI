25905bce5aefded05748247102b3f6a035098b54
Angel D. Munoz <scythes.lol@gmail.com>
2020-01-21 18:44:04 -0700

    add calendar day button bindings

---
+++ src/Avalonia.FuncUI.DSL/Calendar/CalendarButton.fs
@@ -0,0 +1,14 @@
+﻿namespace Avalonia.FuncUI.DSL.Primitives
+
+
+[<AutoOpen>]
+module CalendarButton =
+    open Avalonia.Controls.Primitives
+    open Avalonia.FuncUI.Types
+    open Avalonia.FuncUI.Builder
+    
+    let create (attrs: IAttr<CalendarButton> list): IView<CalendarButton> =
+        ViewBuilder.Create<CalendarButton>(attrs)
+     
+    type CalendarButton with
+        end
