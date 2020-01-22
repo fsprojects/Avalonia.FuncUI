5121b9e46d35790f20f251e3a743e2a80bd57c92
Angel D. Munoz <scythes.lol@gmail.com>
2020-01-22 08:32:15 -0700

    added selected date subscription added dropdown open subscription

--- src/Avalonia.FuncUI.DSL/Calendar/DatePicker.fs
+++ src/Avalonia.FuncUI.DSL/Calendar/DatePicker.fs
@@ -18,20 +18,44 @@ module DatePicker =
         static member displayDateStart<'t when 't :> DatePicker>(value: DateTime Nullable) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<DateTime Nullable>(DatePicker.DisplayDateStartProperty, value, ValueNone)
 
+        static member displayDateStart<'t when 't :> DatePicker>(value: DateTime) : IAttr<'t> =
+            value |> Nullable |> DatePicker.displayDateStart
+
+        static member displayDateStart<'t when 't :> DatePicker>(value: DateTime option) : IAttr<'t> =
+            value |> Option.toNullable |> DatePicker.displayDateStart
+
         static member displayDateEnd<'t when 't :> DatePicker>(value: DateTime Nullable) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<DateTime Nullable>(DatePicker.DisplayDateEndProperty, value, ValueNone)
+
+        static member displayDateEnd<'t when 't :> DatePicker>(value: DateTime) : IAttr<'t> =
+            value |> Nullable |> DatePicker.displayDateEnd
+
+        static member displayDateEnd<'t when 't :> DatePicker>(value: DateTime option) : IAttr<'t> =
+            value |> Option.toNullable |> DatePicker.displayDateEnd
 
         static member firstDayOfWeek<'t when 't :> DatePicker>(value: DayOfWeek) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<DayOfWeek>(DatePicker.FirstDayOfWeekProperty, value, ValueNone)
 
         static member isDropdownOpen<'t when 't :> DatePicker>(value: bool) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<bool>(DatePicker.IsDropDownOpenProperty, value, ValueNone)
+
+        static member onDrdownOpenChanged<'t when 't :> DatePicker>(func: (bool -> unit)) : IAttr<'t> =
+            AttrBuilder<'t>.CreateSubscription(DatePicker.IsDropDownOpenProperty, func)
         
         static member isTodayHighlightedProperty<'t when 't :> DatePicker>(value: bool) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<bool>(DatePicker.IsTodayHighlightedProperty , value, ValueNone)
 
         static member selectedDate<'t when 't :> DatePicker>(value: DateTime Nullable) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<DateTime Nullable>(DatePicker.SelectedDateProperty, value, ValueNone)
+
+        static member selectedDate<'t when 't :> DatePicker>(value: DateTime) : IAttr<'t> =
+            value |> Nullable |> DatePicker.selectedDate
+
+        static member selectedDate<'t when 't :> DatePicker>(value: DateTime option) : IAttr<'t> =
+            value |> Option.toNullable |> DatePicker.selectedDate
+
+        static member onSelectedDateChanged<'t when 't :> DatePicker>(func: (DateTime Nullable -> unit)) : IAttr<'t> =
+            AttrBuilder<'t>.CreateSubscription<DateTime Nullable>(DatePicker.SelectedDateProperty, func)
 
         static member selectedDateFormat<'t when 't :> DatePicker>(value: DatePickerFormat) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<DatePickerFormat>(DatePicker.SelectedDateFormatProperty, value, ValueNone)
