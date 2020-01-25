a072d7d4a6fa6cadb184d7bf425862a18957a98c
Josua Jäger <jaeger.josua@gmail.com>
2020-01-25 11:22:28 +0100

    Update Library.fs

--- src/Avalonia.FuncUI.Elmish/Library.fs
+++ src/Avalonia.FuncUI.Elmish/Library.fs
@@ -3,12 +3,20 @@
 open Elmish
 open Avalonia.FuncUI.Components.Hosts
 open Avalonia.FuncUI.Types
+open Avalonia.Threading
 
 module Program =
 
+    let private syncDispatch (dispatch: Dispatch<'msg>) : Dispatch<'msg> =
+        match Dispatcher.UIThread.CheckAccess() with
+        | true -> fun msg -> Dispatcher.UIThread.Post (fun () -> dispatch msg)
+        | false -> fun msg -> dispatch msg
+    
     let withHost (host: IViewHost) (program: Program<'arg, 'model, 'msg, #IView>) =
         let setState state dispatch =
             let view = ((Program.view program) state dispatch)
             host.Update (Some (view :> IView))
 
-        program |> Program.withSetState setState
+        program
+        |> Program.withSetState setState
+        |> Program.withSyncDispatch syncDispatch
