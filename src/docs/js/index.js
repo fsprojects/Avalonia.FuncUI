5da6f8ddaff4c062c915e00e771a410dffad2ea3
Josua Jäger <jaeger.josua@gmail.com>
2020-04-18 13:46:48 +0200

    move docs to own repo

--- src/docs/js/index.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const navbarmenu = document.querySelector('[data-target=navbarMenu]');
-  const menu = document.querySelector('#navbarMenu');
-  navbarmenu.addEventListener('click', () => {
-    navbarmenu.classList.toggle('is-active');
-    menu.classList.toggle('is-active');
-  });
-});
