d8e4db82c8a5c46248f9a55d2cdcf359bc8563be
FoggyFinder <FoggyFinder@yandex.ua>
2020-07-09 13:49:30 +0300

    add horizontalContentAlignment & verticalContentAlignment to ComboBox; rename files

--- src/Avalonia.FuncUI.DSL/ComboBoxItem.fs
+++ src/Avalonia.FuncUI.DSL/ComboBoxItem.fs
@@ -1,21 +1,13 @@
 namespace Avalonia.FuncUI.DSL
 
 [<AutoOpen>]
-module ComboBox =
+module ComboBoxItem =
     open Avalonia.Controls
     open Avalonia.FuncUI.Types
     open Avalonia.FuncUI.Builder
     
-    let create (attrs: IAttr<ComboBox> list): IView<ComboBox> =
-        ViewBuilder.Create<ComboBox>(attrs)
+    let create (attrs: IAttr<ComboBoxItem> list): IView<ComboBoxItem> =
+        ViewBuilder.Create<ComboBoxItem>(attrs)
     
-    type ComboBox with
-    
-        static member isDropDownOpen<'t when 't :> ComboBox>(value: bool) : IAttr<'t> =
-            AttrBuilder<'t>.CreateProperty<bool>(ComboBox.IsDropDownOpenProperty, value, ValueNone)
-            
-        static member maxDropDownHeight<'t when 't :> ComboBox>(height: float) : IAttr<'t> =
-            AttrBuilder<'t>.CreateProperty<float>(ComboBox.MaxDropDownHeightProperty, height, ValueNone)
-            
-        static member virtualizationMode<'t when 't :> ComboBox>(mode: ItemVirtualizationMode) : IAttr<'t> =
-            AttrBuilder<'t>.CreateProperty<ItemVirtualizationMode>(ComboBox.VirtualizationModeProperty, mode, ValueNone)
+    type ComboBoxItem with
+        end
