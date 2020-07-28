3c3dc348edd4b022d91edfb646434ecfcd90a53e
Renngar <renngar@renngar.com>
2020-07-27 20:40:09 -0400

    Add a drag and drop demo
    This may expose a bug in Avalonia.FuncUI.  As soon as you press the
    mouse button on Drag Me you get two PressedPointerEvents.  This makes
    certain cases not work properly. For example,
    
    1. When you immediately release the button you do not get a
       DragDropEffects.None cancellation response.
    
    2. If you drag it to the drop target, you get a count of 1 the first
       time and n + 2 for each subsequent attempt.
    
    If you look at the output in the terminal you can see the multiple
    messages being triggered and how the display and state do not match.

---
+++ src/Avalonia.FuncUI.ControlCatalog/Views/Tabs/DragDropDemo.fs
@@ -0,0 +1,116 @@
+namespace Avalonia.FuncUI.ControlCatalog.Views
+
+open System
+open Elmish
+open Avalonia.Controls
+open Avalonia.Input
+open Avalonia.FuncUI
+open Avalonia.FuncUI.DSL
+open Avalonia.FuncUI.Components
+open Avalonia.FuncUI.Elmish
+open Avalonia.Layout
+open Avalonia.Threading
+
+module DragDropDemo =
+    type State =
+        { dropText: string
+          dragText: string
+          dragCount: int }
+
+    let init =
+        { dropText = "Drop some text or files here"
+          dragText = "Drag Me"
+          dragCount = 0 }, Cmd.none
+
+    type Msg =
+        | BeginDrag of PointerPressedEventArgs
+        | Dragged of string
+        | Dropped of string
+
+    let doDrag (e, dragCount) =
+        async {
+            let dragData = DataObject()
+            dragData.Set(DataFormats.Text, sprintf "You have dragged text %d times" dragCount)
+
+            let! result = Dispatcher.UIThread.InvokeAsync<DragDropEffects>
+                              (fun _ -> DragDrop.DoDragDrop(e, dragData, DragDropEffects.Copy)) |> Async.AwaitTask
+            return match result with
+                   | DragDropEffects.Copy -> "The text was copied"
+                   | DragDropEffects.Link -> "The text was linked"
+                   | DragDropEffects.None -> "The drag operation was canceled"
+                   | _ -> "That was unexpected"
+        }
+
+    let update (msg: Msg) (state: State): State * Cmd<_> =
+        match msg with
+        | BeginDrag e ->
+            let dragCount = state.dragCount + 1
+            { state with dragCount = dragCount }, Cmd.OfAsync.perform doDrag (e, dragCount) Dragged
+        | Dragged s -> { state with dragText = s }, Cmd.none
+        | Dropped s -> { state with dropText = s }, Cmd.none
+
+    let view (state: State) (dispatch) =
+        StackPanel.create
+            [ StackPanel.orientation Orientation.Vertical
+              StackPanel.spacing 4.0
+              StackPanel.children
+                  [ TextBlock.create
+                      [ TextBlock.classes [ "h1" ]
+                        TextBlock.text "Drag+Drop" ]
+                    TextBlock.create
+                        [ TextBlock.classes [ "h2" ]
+                          TextBlock.text "Example of Drag+Drop capabilities" ]
+                    StackPanel.create
+                        [ StackPanel.orientation Orientation.Horizontal
+                          StackPanel.margin (0.0, 16.0, 0.0, 0.0)
+                          StackPanel.horizontalAlignment HorizontalAlignment.Center
+                          StackPanel.spacing 16.0
+                          StackPanel.children
+                              [ Border.create
+                                  [ Border.classes [ "drag" ]
+                                    Border.borderThickness 2.0
+                                    Border.padding 16.0
+                                    Border.child (TextBlock.create [ TextBlock.text state.dragText ])
+                                    Border.onPointerPressed (BeginDrag >> dispatch) ]
+                                Border.create
+                                    [ Border.classes [ "drop" ]
+                                      Border.padding 16.0
+                                      DragDrop.allowDrop true
+                                      Border.child
+                                          (TextBlock.create
+                                              [ TextBlock.text state.dropText
+                                                DragDrop.onDrop (fun e ->
+                                                    if e.Data.Contains(DataFormats.Text) then
+                                                        Dropped(e.Data.GetText()) |> dispatch
+                                                    elif e.Data.Contains(DataFormats.FileNames) then
+                                                        Dropped
+                                                            (e.Data.GetFileNames() |> String.concat Environment.NewLine)
+                                                        |> dispatch
+                                                    )
+                                                DragDrop.onDragOver (fun e ->
+                                                    e.DragEffects <-
+                                                        if e.Data.Contains(DataFormats.Text)
+                                                           || e.Data.Contains(DataFormats.FileNames) then
+                                                            e.DragEffects
+                                                            &&& (DragDropEffects.Copy ||| DragDropEffects.Link)
+                                                        else
+                                                            DragDropEffects.None) ]) ] ] ] ] ]
+
+
+    type Host() as this =
+        inherit Hosts.HostControl()
+        do
+            this.Styles.Load "avares://Avalonia.FuncUI.ControlCatalog/Views/Tabs/Styles.xaml"
+
+            /// we use this function because sometimes we dispatch messages
+            /// from another thread
+            let syncDispatch (dispatch: Dispatch<'msg>): Dispatch<'msg> =
+                match Dispatcher.UIThread.CheckAccess() with
+                | true -> fun msg -> Dispatcher.UIThread.Post(fun () -> dispatch msg)
+                | false -> dispatch
+
+            Elmish.Program.mkProgram (fun () -> init) update view
+            |> Program.withHost this
+            |> Program.withSyncDispatch syncDispatch
+            |> Program.withConsoleTrace
+            |> Program.run
