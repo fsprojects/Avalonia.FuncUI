df26092b10144b490ec1bfa0de9285ed9086b84c
sleepyfran <fgzv@outlook.com>
2022-03-12 11:07:36 +0100

    Add components counter example

---
+++ src/Examples/Component Examples/Examples.CounterApp/Program.fs
@@ -0,0 +1,37 @@
+namespace Examples.CounterApp
+
+open Avalonia
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.Themes.Fluent
+open Avalonia.FuncUI.Hosts
+
+type MainWindow() =
+    inherit HostWindow()
+    do
+        base.Title <- "Counter Example"
+        base.Height <- 400.0
+        base.Width <- 400.0
+        base.Content <- Main.view
+
+type App() =
+    inherit Application()
+
+    override this.Initialize() =
+        this.Styles.Add (FluentTheme(baseUri = null, Mode = FluentThemeMode.Dark))
+
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
+            let mainWindow = MainWindow()
+            desktopLifetime.MainWindow <- mainWindow
+        | _ -> ()
+
+module Program =
+
+    [<EntryPoint>]
+    let main(args: string[]) =
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .UseSkia()
+            .StartWithClassicDesktopLifetime(args)
