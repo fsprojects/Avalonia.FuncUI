f4d0736306ea96e1258a369820fb58f6d78f4eab
Angel D. Munoz <scythes.lol@gmail.com>
2022-03-23 14:55:33 -0600

    Add FuncUI templates (#196)
    * add funcui templates
    
    * Update templates and Add FullTemplate components
    
    * add readme files

---
+++ src/Avalonia.FuncUI.Templates/content/BasicMvuTemplate/Counter.fs
@@ -0,0 +1,46 @@
+﻿
+namespace BasicMvuTemplate
+
+module Counter =
+    open Avalonia.Controls
+    open Avalonia.FuncUI.DSL
+    open Avalonia.Layout
+    
+    type State = { count : int }
+    let init = { count = 0 }
+
+    type Msg = Increment | Decrement | Reset
+
+    let update (msg: Msg) (state: State) : State =
+        match msg with
+        | Increment -> { state with count = state.count + 1 }
+        | Decrement -> { state with count = state.count - 1 }
+        | Reset -> init
+    
+    let view (state: State) (dispatch) =
+        DockPanel.create [
+            DockPanel.children [
+                Button.create [
+                    Button.dock Dock.Bottom
+                    Button.onClick (fun _ -> dispatch Reset)
+                    Button.content "reset"
+                ]                
+                Button.create [
+                    Button.dock Dock.Bottom
+                    Button.onClick (fun _ -> dispatch Decrement)
+                    Button.content "-"
+                ]
+                Button.create [
+                    Button.dock Dock.Bottom
+                    Button.onClick (fun _ -> dispatch Increment)
+                    Button.content "+"
+                ]
+                TextBlock.create [
+                    TextBlock.dock Dock.Top
+                    TextBlock.fontSize 48.0
+                    TextBlock.verticalAlignment VerticalAlignment.Center
+                    TextBlock.horizontalAlignment HorizontalAlignment.Center
+                    TextBlock.text (string state.count)
+                ]
+            ]
+        ]
