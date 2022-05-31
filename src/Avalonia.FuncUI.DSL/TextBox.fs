6735c0f0c43809f3bcc52ecdb46041569c187aee
Josua Jaeger <mail@jaggerjo.com>
2022-05-31 21:29:06 +0200

    DSL add TextBox.cornerRadius

--- src/Avalonia.FuncUI.DSL/TextBox.fs
+++ src/Avalonia.FuncUI.DSL/TextBox.fs
@@ -1,28 +1,30 @@
 namespace Avalonia.FuncUI.DSL
 
+open Avalonia
+
 [<AutoOpen>]
-module TextBox =  
+module TextBox =
     open Avalonia.Controls
     open Avalonia.Layout
     open Avalonia.Media.Immutable
     open Avalonia.Media
     open Avalonia.FuncUI.Builder
     open Avalonia.FuncUI.Types
-    
+
     let create (attrs: IAttr<TextBox> list): IView<TextBox> =
         ViewBuilder.Create<TextBox>(attrs)
-    
+
     type TextBox with
 
         static member acceptsReturn<'t when 't :> TextBox>(value: bool) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<bool>(TextBox.AcceptsReturnProperty, value, ValueNone)
 
         static member acceptsTab<'t when 't :> TextBox>(value: bool) : IAttr<'t> =
-            AttrBuilder<'t>.CreateProperty<bool>(TextBox.AcceptsTabProperty, value, ValueNone)        
+            AttrBuilder<'t>.CreateProperty<bool>(TextBox.AcceptsTabProperty, value, ValueNone)
 
         static member caretIndex<'t when 't :> TextBox>(value: int) : IAttr<'t> =
-            AttrBuilder<'t>.CreateProperty<int>(TextBox.CaretIndexProperty, value, ValueNone)                
-                        
+            AttrBuilder<'t>.CreateProperty<int>(TextBox.CaretIndexProperty, value, ValueNone)
+
         static member isReadOnly<'t when 't :> TextBox>(value: bool) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<bool>(TextBox.IsReadOnlyProperty, value, ValueNone)
 
@@ -31,54 +33,72 @@ module TextBox =
 
         static member passwordChar<'t when 't :> TextBox>(value: char) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<char>(TextBox.PasswordCharProperty, value, ValueNone)
-            
+
         static member selectionBrush<'t when 't :> TextBox>(value: IBrush) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<IBrush>(TextBox.SelectionBrushProperty, value, ValueNone)
-            
+
         static member selectionBrush<'t when 't :> TextBox>(color: string) : IAttr<'t> =
             color |> Color.Parse |> ImmutableSolidColorBrush |> TextBox.selectionBrush
-            
+
         static member selectionForegroundBrush<'t when 't :> TextBox>(value: IBrush) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<IBrush>(TextBox.SelectionForegroundBrushProperty, value, ValueNone)
-            
+
         static member selectionForegroundBrush<'t when 't :> TextBox>(color: string) : IAttr<'t> =
-            color |> Color.Parse |> ImmutableSolidColorBrush |> TextBox.selectionForegroundBrush       
-            
+            color |> Color.Parse |> ImmutableSolidColorBrush |> TextBox.selectionForegroundBrush
+
         static member caretBrush<'t when 't :> TextBox>(value: IBrush) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<IBrush>(TextBox.CaretBrushProperty, value, ValueNone)
-            
+
         static member caretBrush<'t when 't :> TextBox>(color: string) : IAttr<'t> =
             color |> Color.Parse |> ImmutableSolidColorBrush |> TextBox.selectionBrush
-            
+
         static member selectionStart<'t when 't :> TextBox>(value: int) : IAttr<'t> =
-            AttrBuilder<'t>.CreateProperty<int>(TextBox.SelectionStartProperty, value, ValueNone)     
-            
+            AttrBuilder<'t>.CreateProperty<int>(TextBox.SelectionStartProperty, value, ValueNone)
+
         static member selectionEnd<'t when 't :> TextBox>(value: int) : IAttr<'t> =
-            AttrBuilder<'t>.CreateProperty<int>(TextBox.SelectionEndProperty, value, ValueNone)     
-            
+            AttrBuilder<'t>.CreateProperty<int>(TextBox.SelectionEndProperty, value, ValueNone)
+
         static member maxLength<'t when 't :> TextBox>(value: int) : IAttr<'t> =
-            AttrBuilder<'t>.CreateProperty<int>(TextBox.MaxLengthProperty, value, ValueNone)     
-            
+            AttrBuilder<'t>.CreateProperty<int>(TextBox.MaxLengthProperty, value, ValueNone)
+
         static member text<'t when 't :> TextBox>(value: string) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<string>(TextBox.TextProperty, value, ValueNone)
-            
+
         static member onTextChanged<'t when 't :> TextBox>(func: string -> unit, ?subPatchOptions) =
             AttrBuilder<'t>.CreateSubscription<string>(TextBox.TextProperty, func, ?subPatchOptions = subPatchOptions)
-            
+
         static member textAlignment<'t when 't :> TextBox>(alignment: TextAlignment) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<TextAlignment>(TextBox.TextAlignmentProperty, alignment, ValueNone)
-            
+
         static member textWrapping<'t when 't :> TextBox>(value: TextWrapping) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<TextWrapping>(TextBox.TextWrappingProperty, value, ValueNone)
-            
+
         static member useFloatingWatermark<'t when 't :> TextBox>(value: bool) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<bool>(TextBox.UseFloatingWatermarkProperty, value, ValueNone)
-            
+
         static member newLine<'t when 't :> TextBox>(value: string) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<string>(TextBox.NewLineProperty, value, ValueNone)
 
         static member verticalContentAlignment<'t when 't :> TextBox>(value: VerticalAlignment) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<VerticalAlignment>(TextBox.VerticalContentAlignmentProperty, value, ValueNone)
-            
+
         static member watermark<'t when 't :> TextBox>(value: string) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<string>(TextBox.WatermarkProperty, value, ValueNone)
+
+        static member cornerRadius<'t when 't :> TextBox>(value: CornerRadius) : IAttr<'t> =
+            AttrBuilder<'t>.CreateProperty<CornerRadius>(TextBox.CornerRadiusProperty, value, ValueNone)
+
+        static member cornerRadius<'t when 't :> TextBox>(value: float) : IAttr<'t> =
+            value
+            |> CornerRadius
+            |> TextBox.cornerRadius
+
+        static member cornerRadius<'t when 't :> TextBox>(horizontal: float, vertical: float) : IAttr<'t> =
+            (horizontal, vertical)
+            |> CornerRadius
+            |> TextBox.cornerRadius
+
+        static member cornerRadius<'t when 't :> TextBox>(left: float, top: float, right: float, bottom: float) : IAttr<'t> =
+            (left, top, right, bottom)
+            |> CornerRadius
+            |> TextBox.cornerRadius
