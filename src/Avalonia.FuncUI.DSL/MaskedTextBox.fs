403428f84cafad8a295e1345a4843f1d0f36edf9
Hadrian Tang <hadrianwttang@outlook.com>
2022-08-31 23:23:54 +0800

    Remove get only properties

--- src/Avalonia.FuncUI.DSL/MaskedTextBox.fs
+++ src/Avalonia.FuncUI.DSL/MaskedTextBox.fs
@@ -24,7 +24,7 @@ module MaskedTextBox =
       /// Default value is false.
       /// </summary>
       static member asciiOnly<'t when 't :> MaskedTextBox>(value: bool) : IAttr<'t> =
-            AttrBuilder<'t>.CreateProperty<bool>(MaskedTextBox.AsciiOnlyProperty, value, ValueNone)
+             AttrBuilder<'t>.CreateProperty<bool>(MaskedTextBox.AsciiOnlyProperty, value, ValueNone)
       /// <summary>
       /// Sets the culture information associated with the masked text box.
       /// </summary>
@@ -40,16 +40,6 @@ module MaskedTextBox =
       /// </summary>
       static member mask<'t when 't :> MaskedTextBox>(value: string) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<string>(MaskedTextBox.MaskProperty, value, ValueNone)
-      /// <summary>
-      /// Specifies whether the test string required input positions, as specified by the mask, have all been assigned.
-      /// </summary>
-      static member maskCompleted<'t when 't :> MaskedTextBox>(value: System.Nullable<bool>) : IAttr<'t> =
-            AttrBuilder<'t>.CreateProperty<System.Nullable<bool>>(MaskedTextBox.MaskCompletedProperty, value, ValueNone)
-      /// <summary>
-      /// Specifies whether all inputs (required and optional) have been provided into the mask successfully.
-      /// </summary>
-      static member maskFull<'t when 't :> MaskedTextBox>(value: System.Nullable<bool>) : IAttr<'t> =
-            AttrBuilder<'t>.CreateProperty<System.Nullable<bool>>(MaskedTextBox.MaskFullProperty, value, ValueNone)
       /// <summary>
       /// Sets the character to be displayed in substitute for user input.
       /// </summary>
