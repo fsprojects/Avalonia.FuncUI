98f9780cd78b28fe5bb75df26e31825cf75acdcb
Richard Webb <richard.webb@helpsystems.com>
2022-09-08 09:42:08 +0100

    Add onSelectedItemChanged to TreeView

--- src/Avalonia.FuncUI.DSL/TreeView.fs
+++ src/Avalonia.FuncUI.DSL/TreeView.fs
@@ -19,13 +19,19 @@ module TreeView =
             AttrBuilder<'t>.CreateProperty<bool>(TreeView.AutoScrollToSelectedItemProperty, value, ValueNone)
         
         /// <summary>
-        /// Sets the selected items.
+        /// Subscribes to changes in the SelectedItem property.
+        /// </summary>
+        static member onSelectedItemChanged<'t when 't :> TreeView>(func: obj -> unit, ?subPatchOptions) =
+            AttrBuilder<'t>.CreateSubscription<obj>(TreeView.SelectedItemProperty, func, ?subPatchOptions = subPatchOptions)
+
+        /// <summary>
+        /// Sets the selected item.
         /// </summary>
         static member selectedItem<'t when 't :> TreeView>(value: obj) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<obj>(TreeView.SelectedItemProperty, value, ValueNone)
          
         /// <summary>
-        /// Sets the selected item.
+        /// Sets the selected items.
         /// </summary>
         static member selectedItems<'t when 't :> TreeView>(value: IList) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<IList>(TreeView.SelectedItemsProperty, value, ValueNone)
