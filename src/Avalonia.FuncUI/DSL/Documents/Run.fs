710cc096c36c9532a3fd3635a0116836e15f73d6
sleepyfran <fgzv@outlook.com>
2022-10-15 16:52:24 +0200

    Add styling via TextElement

--- src/Avalonia.FuncUI/DSL/Documents/Run.fs
+++ src/Avalonia.FuncUI/DSL/Documents/Run.fs
@@ -4,7 +4,6 @@ namespace Avalonia.FuncUI.DSL
 module Run =  
     open Avalonia.FuncUI.Builder
     open Avalonia.FuncUI.Types
-    open Avalonia.Media
     open Avalonia.Controls.Documents
 
     let create (attrs: IAttr<Run> list): IView<Run> =
@@ -13,6 +12,3 @@ module Run =
     type Run with
         static member text<'t when 't :> Run>(value: string) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<string>(Run.TextProperty, value, ValueNone)
-            
-        static member background<'t when 't :> Run>(value: IBrush) : IAttr<'t> =
-            AttrBuilder<'t>.CreateProperty<IBrush>(Run.BackgroundProperty, value, ValueNone)
