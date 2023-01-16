b0ddda4e16f5803700667b7e23c327025d6b3af4
JaggerJo <ail@jaggerjo.com>
2023-01-16 21:32:44 +0100

    State.sequenceBy

--- src/Avalonia.FuncUI.UnitTests/State.fs
+++ src/Avalonia.FuncUI.UnitTests/State.fs
@@ -16,7 +16,7 @@ module StateTests =
 
     [<Fact>]
     let ``State.unique`` () =
-        let calls = new ResizeArray<int>()
+        let calls = ResizeArray<int>()
         let origin: IWritable<int> = new State<_>(0)
 
         let _ =
@@ -30,3 +30,70 @@ module StateTests =
         Assert.Equal([1; 2; 3; 1; 6;], calls)
 
         ()
+
+    type private Account =
+        { Name: string
+          Balance: int }
+
+    [<Fact>]
+    let ``State.sequenceBy`` () =
+        let callsToAlice = ResizeArray<Account>()
+        let callsToBob = ResizeArray<Account>()
+
+        let accounts: IWritable<Account list> = new State<_>(List.empty)
+
+        accounts.Set [
+            { Account.Name = "Alice"; Balance = 100 }
+        ]
+
+        let aliceWire =
+            accounts
+            |> State.sequenceBy (fun a -> a.Name)
+            |> List.item 0
+
+        let _ = State.subscribe callsToAlice.Add aliceWire
+
+        accounts.Set [
+            yield! accounts.Current
+            { Account.Name = "Bob"; Balance = 0 }
+        ]
+
+        let _ =
+            accounts
+            |> State.sequenceBy (fun a -> a.Name)
+            |> List.item 1
+            |> State.subscribe callsToBob.Add
+
+        (* Alice gets a deposit (via toplevel) *)
+        accounts.Set [
+            { Account.Name = "Alice"; Balance = 99 }
+            { Account.Name = "Bob"; Balance = 0 }
+        ]
+
+        Assert.Equal (2, callsToAlice.Count)
+        Assert.Equal ({ Account.Name = "Alice"; Balance = 99 }, Seq.last callsToAlice)
+
+        Assert.Equal (1, callsToBob.Count)
+        Assert.Equal ({ Account.Name = "Bob"; Balance = 0 }, Seq.last callsToBob)
+
+        (* Alice gets more money (via new sequenced wire) *)
+        accounts
+        |> State.sequenceBy (fun a -> a.Name)
+        |> List.item 0
+        |> fun state -> state.Set { state.Current with Balance = 101 }
+
+        Assert.Equal (3, callsToAlice.Count)
+        Assert.Equal ({ Account.Name = "Alice"; Balance = 101 }, Seq.last callsToAlice)
+
+        Assert.Equal (2, callsToBob.Count)
+        Assert.Equal ({ Account.Name = "Bob"; Balance = 0 }, Seq.last callsToBob)
+
+        (* Alice gets more money (via old wire *)
+        aliceWire
+        |> fun state -> state.Set { state.Current with Balance = 102 }
+
+        Assert.Equal (4, callsToAlice.Count)
+        Assert.Equal ({ Account.Name = "Alice"; Balance = 102 }, Seq.last callsToAlice)
+
+        Assert.Equal (3, callsToBob.Count)
+        Assert.Equal ({ Account.Name = "Bob"; Balance = 0 }, Seq.last callsToBob)
