3e16d87295d649ead93d719dba2296078a22a201
Josua Jaeger <mail@jaggerjo.com>
2023-01-29 16:32:29 +0100

    add single perf test

---
+++ src/Avalonia.FuncUI.PerfTests/Program.fs
@@ -0,0 +1,8 @@
+﻿open System
+open BenchmarkDotNet.Running
+open Avalonia.FuncUI.PerfTests
+
+[<EntryPoint>]
+let main argv =
+    BenchmarkRunner.Run<VirtualDomCreationBenchmarks>() |> ignore
+    0 // return an integer exit code
