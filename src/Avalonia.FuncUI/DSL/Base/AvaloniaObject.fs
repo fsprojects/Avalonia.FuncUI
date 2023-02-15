2aeb7ef2cb16b68cf49d098ac0be849721242d30
Josua Jaeger <mail@jaggerjo.com>
2023-02-15 17:12:34 +0100

    cleanup

--- src/Avalonia.FuncUI/DSL/Base/AvaloniaObject.fs
+++ src/Avalonia.FuncUI/DSL/Base/AvaloniaObject.fs
@@ -1,18 +1,13 @@
-namespace Avalonia.FuncUI
+namespace Avalonia.FuncUI.DSL
 
-open System.ComponentModel
 open Avalonia
-open Avalonia.Controls
-open Avalonia.Data
+open Avalonia.FuncUI
 open Avalonia.FuncUI.Types
-open Avalonia.Styling
-open System.Runtime.CompilerServices
-open System.Runtime.InteropServices
 
 [<AutoOpen>]
-module __Bindable =
+module AvaloniaObject =
 
-    type Control with
+    type IAvaloniaObject with
 
         /// <summary>
         /// Hook into the controls lifetime. This is called when the backing avalonia control is created.
@@ -31,35 +26,15 @@ module __Bindable =
         /// </code>
         /// </example>
         /// </summary>
-        static member init<'t when 't :> Control>(func: 't -> unit) : IAttr<'t> =
+        static member init<'t when 't :> AvaloniaObject>(func: 't -> unit) : IAttr<'t> =
             Attr.InitFunction {
                 InitFunction.Function = (fun (control: obj) -> func (control :?> 't))
             }
 
-    type IAvaloniaObject with
-
-        member this.Bind(prop: DirectPropertyBase<'value>, readable: IReadable<'value>) : unit =
+        member this.Bind(prop: DirectPropertyBase<'value>, readable: #IReadable<'value>) : unit =
             let _ = this.Bind(property = prop, source = readable.ImmediateObservable)
             ()
 
-        member this.Bind(prop: StyledPropertyBase<'value>, readable: IReadable<'value>) : unit =
+        member this.Bind(prop: StyledPropertyBase<'value>, readable: #IReadable<'value>) : unit =
             let _ = this.Bind(property = prop, source = readable.ImmediateObservable)
             ()
-
-[<AbstractClass>]
-type StaticComponent () as this =
-    inherit Border ()
-
-    override this.OnInitialized () =
-        base.OnInitialized ()
-
-        this.DataContext <- this
-        this.Child <-
-            ()
-            |> this.Build
-            |> VirtualDom.VirtualDom.create
-
-    abstract member Build: unit -> IView
-
-    interface IStyleable with
-        member this.StyleKey = typeof<Border>
