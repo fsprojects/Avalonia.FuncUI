389d0d91c9e25e5b6123f18ce648385b421762c5
Richard Webb <richard.webb@helpsystems.com>
2023-02-15 17:13:12 +0000

    Add a WebAssembly control catalog project

---
+++ src/Avalonia.FuncUI.ControlCatalog/Avalonia.FuncUI.ControlCatalog.Browser/Program.fs
@@ -0,0 +1,20 @@
+
+open System.Runtime.Versioning
+open Avalonia
+open Avalonia.Browser
+
+module public Program =
+    [<assembly: SupportedOSPlatform("browser")>]
+    do ()
+
+    [<CompiledName "BuildAvaloniaApp">] 
+    let public buildAvaloniaApp () = 
+        AppBuilder
+            .Configure<Avalonia.FuncUI.ControlCatalog.App>()
+
+    [<EntryPoint>]
+    let main argv =
+        buildAvaloniaApp()
+            .SetupBrowserApp("out")
+            |> ignore
+        0
