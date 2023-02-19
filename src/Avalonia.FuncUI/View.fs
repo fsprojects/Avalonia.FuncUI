28c2656ab65acf78166659364b84b0b3e078fc83
Josua Jaeger <mail@jaggerjo.com>
2023-02-19 19:55:37 +0100

    add doc comments

--- src/Avalonia.FuncUI/View.fs
+++ src/Avalonia.FuncUI/View.fs
@@ -5,14 +5,36 @@ open Avalonia.FuncUI.Types
 [<AbstractClass; Sealed>]
 type View () =
 
-    static member createGeneric<'t> (attrs: IAttr<'t> list) =
-        { View.ViewType = typeof<'t>
+    /// <summary>
+    /// Creates a new IView&lt;'t&gt; with the given attributes.
+    /// <example>
+    /// <code>
+    /// View.createGeneric&lt;Button&gt; [
+    ///     Button.content "Click me"
+    ///     ..
+    /// ]
+    /// </code>
+    /// </example>
+    /// </summary>
+    static member createGeneric<'view> (attrs: IAttr<'view> list) =
+        { View.ViewType = typeof<'view>
           View.ViewKey = ValueNone
           View.Attrs = attrs
           View.ConstructorArgs = null
           View.Outlet = ValueNone }
-        :> IView<'t>
+        :> IView<'view>
 
+    /// <summary>
+    /// Creates a new IView&lt;'view&gt; with the given attributes and key.
+    /// <example>
+    /// <code>
+    /// View.createWithKey "key" Button.create [
+    ///     Button.content "Click me"
+    ///     ..
+    /// ]
+    /// </code>
+    /// </example>
+    /// </summary>
     static member createWithKey (key: string) (createView: IAttr<'view> list -> IView<'view>) (attrs: IAttr<'view> list) =
         let view = createView(attrs)
 
@@ -23,6 +45,21 @@ type View () =
           View.Outlet = ValueNone }
         :> IView<'view>
 
+    /// <summary>
+    /// Creates a new IView&lt;'view&gt; with the given attributes and outlet.
+    /// <example>
+    /// <code>
+    ///
+    /// let mutable button: Button = null
+    ///
+    /// View.createWithOutlet (fun btnRef -&gt; button &lt;- btnRef) Button.create [
+    ///     Button.content "Click me"
+    ///     ..
+    /// ]
+    /// </code>
+    /// </example>
+    /// <remarks>Consider using an init attribute instead.</remarks>
+    /// </summary>
     static member createWithOutlet (outlet: 'view -> unit) (createView: IAttr<'view> list -> IView<'view>) (attrs: IAttr<'view> list) =
         let view = createView(attrs)
 
@@ -33,6 +70,19 @@ type View () =
           View.Outlet = ValueSome (fun control -> outlet (control :?> 'view)) }
         :> IView<'view>
 
+    /// <summary>
+    /// Creates a new IView&lt;'view&gt; with the given attributes and key.
+    /// <example>
+    /// <code>
+    /// [
+    ///     Button.content "Click me"
+    ///     ..
+    /// ]
+    /// |&gt; Button.create
+    /// |&gt; View.withKey "key"
+    /// </code>
+    /// </example>
+    /// </summary>
     static member withKey (key: string) (view: IView<'view>) : IView<'view> =
         { View.ViewType = view.ViewType
           View.ViewKey = ValueSome key
@@ -40,6 +90,23 @@ type View () =
           View.ConstructorArgs = view.ConstructorArgs
           View.Outlet = view.Outlet }
 
+     /// <summary>
+    /// Creates a new IView&lt;'view&gt; with the given attributes and outlet.
+    /// <example>
+    /// <code>
+    ///
+    /// let mutable button: Button = null
+    ///
+    /// [
+    ///     Button.content "Click me"
+    ///     ..
+    /// ]
+    /// |&gt; Button.create
+    /// |&gt; View.withOutlet (fun btnRef -&gt; button &lt;- btnRef)
+    /// </code>
+    /// </example>
+    /// <remarks>Consider using an init attribute instead.</remarks>
+    /// </summary>
     static member withOutlet (outlet: 'view -> unit) (view: IView<'view>) : IView<'view> =
         { View.ViewType = view.ViewType
           View.ViewKey = view.ViewKey
@@ -47,6 +114,18 @@ type View () =
           View.ConstructorArgs = view.ConstructorArgs
           View.Outlet = ValueSome (fun control -> outlet (control :?> 'view)) }
 
+    /// <summary>
+    /// Creates a new IView&lt;'view&gt; with constructor arguments.
+    /// <example>
+    /// <code>
+    /// [
+    ///     ...
+    /// ]
+    /// |&gt; MyView.create
+    /// |&gt; View.withConstructorArgs [| arg1 :&gt; obj; arg1 :&gt; obj; |]
+    /// </code>
+    /// </example>
+    /// </summary>
     static member withConstructorArgs (constructorArgs: obj array) (view: IView<'view>) : IView<'view> =
         { View.ViewType = view.ViewType
           View.ViewKey = view.ViewKey
