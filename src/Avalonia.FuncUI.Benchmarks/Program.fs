f93feda2d08d237a4b05a0eda216e623111f7d93
Josua Jaeger <mail@jaggerjo.com>
2023-04-08 21:05:49 +0200

    add empty benchmark project

---
+++ src/Avalonia.FuncUI.Benchmarks/Program.fs
@@ -0,0 +1,22 @@
+﻿namespace Avalonia.FuncUI.Benchmarks
+
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Running
+
+[<MemoryDiagnoser; >]
+type Bench () =
+
+    [<Benchmark(Baseline = true)>]
+    member this.Baseline () : unit =
+        ()
+
+    [<Benchmark>]
+    member this.Champion () : unit =
+        ()
+
+module Program =
+
+    [<EntryPoint>]
+    let main (args: string array) : int =
+        let _ = BenchmarkRunner.Run<Bench>()
+        0
--- src/Avalonia.FuncUI/Assembly.fs
+++ src/Avalonia.FuncUI/Assembly.fs
@@ -3,5 +3,6 @@ namespace Avalonia.FuncUI.Assembly
 open System.Runtime.CompilerServices
 
 [<assembly: InternalsVisibleTo("Avalonia.FuncUI.UnitTests")>]
+[<assembly: InternalsVisibleTo("Avalonia.FuncUI.Benchmarks")>]
 [<assembly: InternalsVisibleTo("Avalonia.FuncUI.Diagnostics")>]
 do()
