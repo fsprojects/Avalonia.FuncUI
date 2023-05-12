fc98ce7c6dfa1e5f4ac5ab47ac49f54202234f26
Josua Jaeger <mail@jaggerjo.com>
2023-05-12 17:18:31 +0200

    add bindings for all render options

--- src/Avalonia.FuncUI/DSL/Base/RenderOptions.fs
+++ src/Avalonia.FuncUI/DSL/Base/RenderOptions.fs
@@ -20,5 +20,33 @@ module RenderOptions =
                 comparer = ValueNone
             )
 
+        static member edgeMode<'t when 't :> Visual>(mode: EdgeMode) : IAttr<'t> =
+            AttrBuilder<'t>.CreateProperty<EdgeMode>(
+                name = nameof EdgeMode,
+                value = mode,
+                getter = ValueSome RenderOptions.GetEdgeMode,
+                setter = ValueSome RenderOptions.SetEdgeMode,
+                comparer = ValueNone
+            )
+
+        static member bitmapBlendingMode<'t when 't :> Visual>(mode: BitmapBlendingMode) : IAttr<'t> =
+            AttrBuilder<'t>.CreateProperty<BitmapBlendingMode>(
+                name = nameof BitmapBlendingMode,
+                value = mode,
+                getter = ValueSome RenderOptions.GetBitmapBlendingMode,
+                setter = ValueSome RenderOptions.SetBitmapBlendingMode,
+                comparer = ValueNone
+            )
+
+        static member textRenderingMode<'t when 't :> Visual>(mode: TextRenderingMode) : IAttr<'t> =
+            AttrBuilder<'t>.CreateProperty<TextRenderingMode>(
+                name = nameof TextRenderingMode,
+                value = mode,
+                getter = ValueSome RenderOptions.GetTextRenderingMode,
+                setter = ValueSome RenderOptions.SetTextRenderingMode,
+                comparer = ValueNone
+            )
+
+
     type RenderOptions with
         end
