4a28ecbd2a8e33a45977730e7027ec18d07ea94c
Josua Jaeger <mail@jaggerjo.com>
2023-05-12 17:14:53 +0200

    fix 'bitmapInterpolationMode'

--- src/Avalonia.FuncUI/DSL/Base/RenderOptions.fs
+++ src/Avalonia.FuncUI/DSL/Base/RenderOptions.fs
@@ -1,16 +1,24 @@
 namespace Avalonia.FuncUI.DSL
 
+open Avalonia
+
 [<AutoOpen>]
-module RenderOptions =  
-    open Avalonia.Controls
+module RenderOptions =
     open Avalonia.Media
     open Avalonia.Media.Imaging
     open Avalonia.FuncUI.Types
     open Avalonia.FuncUI.Builder
-    
-    type Control with
-        static member bitmapInterpolationMode<'t when 't :> Control>(mode: BitmapInterpolationMode) : IAttr<'t> =
-            AttrBuilder<'t>.CreateProperty<BitmapInterpolationMode>(RenderOptions.BitmapInterpolationModeProperty, mode, ValueNone)
-    
+
+    type Visual with
+
+        static member bitmapInterpolationMode<'t when 't :> Visual>(mode: BitmapInterpolationMode) : IAttr<'t> =
+            AttrBuilder<'t>.CreateProperty<BitmapInterpolationMode>(
+                name = nameof BitmapInterpolationMode,
+                value = mode,
+                getter = ValueSome RenderOptions.GetBitmapInterpolationMode,
+                setter = ValueSome RenderOptions.SetBitmapInterpolationMode,
+                comparer = ValueNone
+            )
+
     type RenderOptions with
         end
