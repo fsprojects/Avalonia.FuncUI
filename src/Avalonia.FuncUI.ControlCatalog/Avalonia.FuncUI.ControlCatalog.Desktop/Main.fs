a81e8f0711500097086d4366845f9e378980cdbd
Richard Webb <webby@beardmouse.co.uk>
2023-05-30 14:21:41 +0100

    Drag and Drop functionality (#295)
    * Add drag and drop support
    
    * Add a drag and drop demo
    
    This may expose a bug in Avalonia.FuncUI.  As soon as you press the
    mouse button on Drag Me you get two PressedPointerEvents.  This makes
    certain cases not work properly. For example,
    
    1. When you immediately release the button you do not get a
       DragDropEffects.None cancellation response.
    
    2. If you drag it to the drop target, you get a count of 1 the first
       time and n + 2 for each subsequent attempt.
    
    If you look at the output in the terminal you can see the multiple
    messages being triggered and how the display and state do not match.
    
    * Workaround getting two events when dragging starts
    
    This is an ugly hack to work around the fact that we get to
    PointerPressedEvents when the user presses the mouse button over the
    drag source.  It should be reverted or removed once that is fixed.
    
    * Fix onPointerPressed handling
    
    Marks the event as handled avoiding the ugly hack of tracking drag state
    in the model.
    
    The remaining bug is that occassionally we get a Dragged message without
    receiving a Dropped message. The Dragged message is fired by completion
    of the doDrag Async. I assume that is why we always get one. However the
    Dropped message is dispatched from the drop target's onDrop handler. It
    is possible that this is responsible for the difference.
    
    I tried adding diagnostics to the onDrop handler to see if it was
    receiving and unexpected data type that is not being handled, but it is
    not even being called.
    
    * Update to the current versions of things
    
    ---------
    
    Co-authored-by: Renngar <renngar@renngar.com>
    Co-authored-by: Richard Webb <richard.webb@helpsystems.com>

--- src/Avalonia.FuncUI.ControlCatalog/Avalonia.FuncUI.ControlCatalog.Desktop/Main.fs
+++ src/Avalonia.FuncUI.ControlCatalog/Avalonia.FuncUI.ControlCatalog.Desktop/Main.fs
@@ -1,9 +1,11 @@
 ﻿namespace Avalonia.FuncUI.ControlCatalog
 
 open Avalonia
+open System
 
 module Program =
 
+    [<STAThread>]
     [<EntryPoint>]
     let main(args: string[]) =
         AppBuilder
