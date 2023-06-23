8e588a900e566dc944fe781c0a0ea0a2e0a78f24
Hadrian Tang <hadrianwttang@outlook.com>
2023-06-24 02:40:42 +0800

    Update MainActivity.fs

--- src/Avalonia.FuncUI.Templates/content/CrossPlatformMvuTemplate/AvaApp.Android/MainActivity.fs
+++ src/Avalonia.FuncUI.Templates/content/CrossPlatformMvuTemplate/AvaApp.Android/MainActivity.fs
@@ -4,6 +4,6 @@ open Android.Content.PM
 open Avalonia.Android
 open Avalonia
 
-[<Activity(Label = "AvaApp.Android", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon", LaunchMode = LaunchMode.SingleInstance, ConfigurationChanges = (ConfigChanges.Orientation ||| ConfigChanges.ScreenSize))>]
+[<Activity(Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon", LaunchMode = LaunchMode.SingleInstance, ConfigurationChanges = (ConfigChanges.Orientation ||| ConfigChanges.ScreenSize))>]
 type MainActivity() =
     inherit AvaloniaMainActivity()
