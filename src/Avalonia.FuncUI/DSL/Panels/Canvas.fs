03c82731d57db743dad8f766ddf34c5d8c0e62fa
Hadrian Tang <hadrianwttang@outlook.com>
2023-06-24 17:39:31 +0800

    the

--- src/Avalonia.FuncUI/DSL/Panels/Canvas.fs
+++ src/Avalonia.FuncUI/DSL/Panels/Canvas.fs
@@ -10,19 +10,19 @@ module Canvas =
         ViewBuilder.Create<Canvas>(attrs)
     
     type Control with
-        /// An attached property specifying distance from the left of the parent Canvas. The applied control should be a child of a Canvas for this property to take effect.
+        /// An attached property specifying the distance from the left of the parent Canvas. The applied control should be a child of a Canvas for this property to take effect.
         static member left<'t when 't :> Control>(value: double) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<double>(Canvas.LeftProperty, value, ValueNone)
             
-        /// An attached property specifying distance from the top of the parent Canvas. The applied control should be a child of a Canvas for this property to take effect.
+        /// An attached property specifying the distance from the top of the parent Canvas. The applied control should be a child of a Canvas for this property to take effect.
         static member top<'t when 't :> Control>(value: double) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<double>(Canvas.TopProperty, value, ValueNone)
             
-        /// An attached property specifying distance from the right of the parent Canvas. The applied control should be a child of a Canvas for this property to take effect.
+        /// An attached property specifying the distance from the right of the parent Canvas. The applied control should be a child of a Canvas for this property to take effect.
         static member right<'t when 't :> Control>(value: double) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<double>(Canvas.RightProperty, value, ValueNone)
             
-        /// An attached property specifying distance from the bottom of the parent Canvas. The applied control should be a child of a Canvas for this property to take effect.
+        /// An attached property specifying the distance from the bottom of the parent Canvas. The applied control should be a child of a Canvas for this property to take effect.
         static member bottom<'t when 't :> Control>(value: double) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<double>(Canvas.BottomProperty, value, ValueNone)
     
