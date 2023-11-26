bea9e6e2b0f559612e0d1397b718a943d12826d7
Josua Jaeger <mail@jaggerjo.com>
2023-11-26 11:36:43 +0100

    move `StaticComponent` to experiments namespace.

--- src/Avalonia.FuncUI/Experimantal/Experimental.StaticComponent.fs
+++ src/Avalonia.FuncUI/Experimantal/Experimental.StaticComponent.fs
@@ -1,11 +1,12 @@
-namespace Avalonia.FuncUI
+namespace Avalonia.FuncUI.Experimental
 
-open Avalonia
 open Avalonia.Controls
 open Avalonia.FuncUI.Types
+open Avalonia.FuncUI.VirtualDom
 
+/// A component that only evaluates its build/render function once. All other changes to the UI are driven by
+/// bindings / manually manipulating controls.
 [<AbstractClass>]
-[<Experimental "Statically construct views with F#">]
 type StaticComponent () =
     inherit Border ()
 
@@ -16,7 +17,7 @@ type StaticComponent () =
         this.Child <-
             ()
             |> this.Build
-            |> VirtualDom.VirtualDom.create
+            |> VirtualDom.create
 
     abstract member Build: unit -> IView
 
