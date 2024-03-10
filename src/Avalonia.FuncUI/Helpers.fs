5e41c7f7dd0f27509db52c9310fd2aee8d9a61a6
Josua Jaeger <mail@jaggerjo.com>
2024-03-10 14:00:03 +0100

    add resources Load extension

--- src/Avalonia.FuncUI/Helpers.fs
+++ src/Avalonia.FuncUI/Helpers.fs
@@ -1,12 +1,14 @@
 namespace Avalonia.FuncUI
 
+open Avalonia.Controls
+
 [<AutoOpen>]
 module Helpers =
     open Avalonia.FuncUI.Types
-    
+
     let generalize (view: IView<'t>) : IView =
         view :> IView
-        
+
 [<AutoOpen>]
 module AvaloniaExtensions =
     open Avalonia.Markup.Xaml.Styling
@@ -14,11 +16,42 @@ module AvaloniaExtensions =
     open Avalonia.Styling
 
     type Styles with
-        member this.Load (source: string) = 
+
+        /// <summary>
+        /// Loads a style from a XAML file.
+        /// <remarks>
+        /// TabItem.axaml is a Style in the example below.
+        /// </remarks>
+        /// <example>
+        /// <code>
+        /// this.Styles.Load "avares://MyApp/Assets/Styles/TabItem.axaml"
+        /// </code>
+        /// </example>
+        /// </summary>
+        member this.Load (source: string) =
             let style = StyleInclude(baseUri = null)
             style.Source <- Uri(source)
             this.Add(style)
-            
+
+    type IResourceDictionary with
+
+        /// <summary>
+        /// Loads a resource dictionary from a XAML file.
+        /// <remarks>
+        /// TabItem.axaml is a ResourceDictionary in the example below.
+        /// </remarks>
+        /// <example>
+        /// <code>
+        /// this.Resources.Load "avares://MyApp/Assets/Styles/TabItem.axaml"
+        /// </code>
+        /// </example>
+        /// </summary>
+        member this.Load (source: string) =
+            let resource = ResourceInclude(baseUri = null)
+            resource.Source <- Uri(source)
+            this.MergedDictionaries.Add(resource)
+
+
 module internal EqualityComparers =
     open Avalonia.Media
 
