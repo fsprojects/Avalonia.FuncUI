958a25e55058e02f6c0a0b2d96e2bdc26345ff9c
Richard Webb <richard.webb@helpsystems.com>
2024-04-01 12:30:22 +0100

    Sort the control items in the control catalog into alphabetical order

--- src/Avalonia.FuncUI.ControlCatalog/Avalonia.FuncUI.ControlCatalog/Views/MainView.fs
+++ src/Avalonia.FuncUI.ControlCatalog/Avalonia.FuncUI.ControlCatalog/Views/MainView.fs
@@ -2,7 +2,6 @@
 
 open Avalonia.Controls
 open Avalonia.FuncUI.DSL
-open Avalonia.FuncUI.Builder
 
 module MainView =
 
@@ -26,36 +25,8 @@ module MainView =
             TabControl.tabStripPlacement Dock.Left
             TabControl.viewItems [
                 TabItem.create [
-                    TabItem.header "Data Template Demo"
-                    TabItem.content (DataTemplateDemo.view state.dataTemplateState (DataTemplateDemoMsg >> dispatch))
-                ]
-                TabItem.create [
-                    TabItem.header "Grid Demo"
-                    TabItem.content (ViewBuilder.Create<GridDemo.Host>([]))
-                ]
-                TabItem.create [
-                    TabItem.header "Gridsplitter Demo"
-                    TabItem.content (ViewBuilder.Create<GridSplitterDemo.Host>([]))
-                ]
-                TabItem.create [
-                    TabItem.header "WrapPanel Demo"
-                    TabItem.content (ViewBuilder.Create<WrapPanelDemo.Host>([]))
-                ]
-                TabItem.create [
-                    TabItem.header "ToolTip Demo"
-                    TabItem.content (ViewBuilder.Create<ToolTipDemo.Host>([]))
-                ]
-                TabItem.create [
-                    TabItem.header "TreeView Demo"
-                    TabItem.content (ViewBuilder.Create<TreeViewDemo.Host>([]))
-                ]
-                TabItem.create [
-                    TabItem.header "NumericUpDown Demo"
-                    TabItem.content (ViewBuilder.Create<NumericUpDownDemo.Host>([]))
-                ]
-                TabItem.create [
-                    TabItem.header "Spinner Demo"
-                    TabItem.content (ViewBuilder.Create<SpinnerDemo.Host>([]))
+                    TabItem.header "CalendarDatePicker Demo"
+                    TabItem.content (ViewBuilder.Create<CalendarDatePickerDemo.Host>([]))
                 ]
                 TabItem.create [
                     TabItem.header "Canvas Demo"
@@ -66,8 +37,44 @@ module MainView =
                     TabItem.content (ViewBuilder.Create<ContextMenuDemo.Host>([]))
                 ]
                 TabItem.create [
-                    TabItem.header "Window Menu Demo"
-                    TabItem.content (ViewBuilder.Create<WindowMenuDemo.Host>([]))
+                    TabItem.header "Data Template Demo"
+                    TabItem.content (DataTemplateDemo.view state.dataTemplateState (DataTemplateDemoMsg >> dispatch))
+                ]
+                TabItem.create [
+                    TabItem.header "DatePicker Demo"
+                    TabItem.content (ViewBuilder.Create<DatePickerDemo.Host>([]))
+                ]
+                TabItem.create [
+                    TabItem.header "Drag+Drop Demo"
+                    TabItem.content (ViewBuilder.Create<DragDropDemo.Host>([]))
+                ]
+                TabItem.create [
+                    TabItem.header "Flyout Demo"
+                    TabItem.content (ViewBuilder.Create<FlyoutDemo.Host>([]))
+                ]
+                TabItem.create [
+                    TabItem.header "Grid Demo"
+                    TabItem.content (ViewBuilder.Create<GridDemo.Host>([]))
+                ]
+                TabItem.create [
+                    TabItem.header "Grid patch Demo"
+                    TabItem.content (ViewBuilder.Create<GridPatchDemo.Host>([]))
+                ]
+                TabItem.create [
+                    TabItem.header "Gridsplitter Demo"
+                    TabItem.content (ViewBuilder.Create<GridSplitterDemo.Host>([]))
+                ]
+                TabItem.create [
+                    TabItem.header "NumericUpDown Demo"
+                    TabItem.content (ViewBuilder.Create<NumericUpDownDemo.Host>([]))
+                ]
+                TabItem.create [
+                    TabItem.header "Spinner Demo"
+                    TabItem.content (ViewBuilder.Create<SpinnerDemo.Host>([]))
+                ]
+                TabItem.create [
+                    TabItem.header "SplitView Demo"
+                    TabItem.content (ViewBuilder.Create<SplitViewDemo.Host>([]))
                 ]
                 // ToDo: return it back when styles will be worked
                 //TabItem.create [
@@ -79,40 +86,32 @@ module MainView =
                     TabItem.content (ViewBuilder.Create<TextBoxDemo.Host>([]))
                 ]
                 TabItem.create [
-                    TabItem.header "CalendarDatePicker Demo"
-                    TabItem.content (ViewBuilder.Create<CalendarDatePickerDemo.Host>([]))
-                ]
-                TabItem.create [
-                    TabItem.header "Grid patch Demo"
-                    TabItem.content (ViewBuilder.Create<GridPatchDemo.Host>([]))
-                ]
-                TabItem.create [
-                    TabItem.header "ToggleSwitch Demo"
-                    TabItem.content (ViewBuilder.Create<ToggleSwitchDemo.Host>([]))
-                ]
-                TabItem.create [
-                    TabItem.header "DatePicker Demo"
-                    TabItem.content (ViewBuilder.Create<DatePickerDemo.Host>([]))
+                    TabItem.header "TickBar Demo"
+                    TabItem.content (ViewBuilder.Create<TickBarDemo.Host>([]))
                 ]
                 TabItem.create [
                     TabItem.header "TimePicker Demo"
                     TabItem.content (ViewBuilder.Create<TimePickerDemo.Host>([]))
                 ]
                 TabItem.create [
-                    TabItem.header "SplitView Demo"
-                    TabItem.content (ViewBuilder.Create<SplitViewDemo.Host>([]))
+                    TabItem.header "ToggleSwitch Demo"
+                    TabItem.content (ViewBuilder.Create<ToggleSwitchDemo.Host>([]))
                 ]
                 TabItem.create [
-                    TabItem.header "TickBar Demo"
-                    TabItem.content (ViewBuilder.Create<TickBarDemo.Host>([]))
+                    TabItem.header "ToolTip Demo"
+                    TabItem.content (ViewBuilder.Create<ToolTipDemo.Host>([]))
                 ]
                 TabItem.create [
-                    TabItem.header "Flyout Demo"
-                    TabItem.content (ViewBuilder.Create<FlyoutDemo.Host>([]))
+                    TabItem.header "TreeView Demo"
+                    TabItem.content (ViewBuilder.Create<TreeViewDemo.Host>([]))
                 ]
                 TabItem.create [
-                    TabItem.header "Drag+Drop Demo"
-                    TabItem.content (ViewBuilder.Create<DragDropDemo.Host>([]))
+                    TabItem.header "Window Menu Demo"
+                    TabItem.content (ViewBuilder.Create<WindowMenuDemo.Host>([]))
+                ]
+                TabItem.create [
+                    TabItem.header "WrapPanel Demo"
+                    TabItem.content (ViewBuilder.Create<WrapPanelDemo.Host>([]))
                 ]
             ]
         ]
