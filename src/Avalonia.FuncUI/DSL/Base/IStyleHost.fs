de6c577e386797416d4dde61b346d3fe971c6406
SilkyFowl <16532218+SilkyFowl@users.noreply.github.com>
2024-04-17 19:52:20 +0900

    fix IStyleHost.styles
    - compare list of IStyle correctly
    - setter should also update Resources

--- src/Avalonia.FuncUI/DSL/Base/IStyleHost.fs
+++ src/Avalonia.FuncUI/DSL/Base/IStyleHost.fs
@@ -1,8 +1,5 @@
 namespace Avalonia.FuncUI.DSL
 
-open Avalonia.Controls
-open Avalonia.Controls.Primitives
-
 [<AutoOpen>]
 module IStyleHost =
     open Avalonia.FuncUI.Types
@@ -11,8 +8,6 @@ module IStyleHost =
 
 
     module private Internals =
-        open System.Collections.Generic
-        open System.Linq
 
         let patchStyles (styles: Styles) (newValues: IStyle seq) =
             let newValues = newValues |> Seq.toList
@@ -30,11 +25,6 @@ module IStyleHost =
                     | Some oldIndex -> styles.Move(oldIndex, newIndex)
                     | None -> styles.Insert(newIndex, newStyle)
 
-        let compareStyleSeq<'e when 'e :> seq<IStyle>> (a: obj, b: obj) =
-            match a, b with
-            | (:? 'e as a), (:? 'e as b) -> Enumerable.SequenceEqual(a, b)
-            | _ -> a = b
-
     type IStyleHost with
 
         /// Use 'classes' instead when possible.
@@ -43,34 +33,42 @@ module IStyleHost =
 
             let setter: ('t * Styles -> unit) =
                 (fun (control, value) ->
-                    Internals.patchStyles control.Styles value)
+                    Internals.patchStyles control.Styles value
+                    control.Styles.Resources <- value.Resources)
 
-            AttrBuilder<'t>
-                .CreateProperty<Styles>(
-                    "Styles",
-                    value,
-                    ValueSome getter,
-                    ValueSome setter,
-                    ValueSome Internals.compareStyleSeq<Styles>,
-                    fun () -> Styles()
-                )
+            let compare: (obj * obj -> bool) =
+                fun (a, b) ->
+                    match a, b with
+                    | (:? Styles as a), (:? Styles as b) ->
+                        System.Linq.Enumerable.SequenceEqual(a, b)
+                        && System.Linq.Enumerable.SequenceEqual(a.Resources, b.Resources)
+                        && System.Linq.Enumerable.SequenceEqual(
+                            a.Resources.MergedDictionaries,
+                            b.Resources.MergedDictionaries
+                        )
+                        && System.Linq.Enumerable.SequenceEqual(
+                            a.Resources.ThemeDictionaries,
+                            b.Resources.ThemeDictionaries
+                        )
+                    | _ -> a = b
+
+            let factory = fun () -> Styles()
+
+            AttrBuilder<'t>.CreateProperty<Styles>("Styles", value, ValueSome getter, ValueSome setter, ValueSome compare, factory)
 
         /// Use 'classes' instead when possible.
         static member styles<'t when 't :> IStyleHost>(styles: IStyle list) : IAttr<'t> =
-
             let getter: ('t -> (IStyle list)) = (fun control -> control.Styles |> Seq.toList)
 
             let setter: ('t * IStyle list -> unit) =
                 (fun (control, value) -> Internals.patchStyles control.Styles value)
 
+            let compare: (obj * obj -> bool) =
+                fun (a, b) ->
+                    match a, b with
+                    | (:? list<IStyle> as a), (:? list<IStyle> as b) -> System.Linq.Enumerable.SequenceEqual(a, b)
+                    | _ -> a = b
+
             let factory = fun () -> []
 
-            AttrBuilder<'t>
-                .CreateProperty<IStyle list>(
-                    "Styles",
-                    styles,
-                    ValueSome getter,
-                    ValueSome setter,
-                    ValueSome Internals.compareStyleSeq<list<IStyle>>,
-                    factory
-                )
+            AttrBuilder<'t>.CreateProperty<IStyle list>("Styles", styles, ValueSome getter, ValueSome setter, ValueSome compare, factory)
