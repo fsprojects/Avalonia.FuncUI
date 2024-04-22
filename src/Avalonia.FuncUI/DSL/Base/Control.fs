1f1464b2b1d302aa91171d9cf2986a7c9bcd8ca5
SilkyFowl <16532218+SilkyFowl@users.noreply.github.com>
2024-04-22 16:12:41 +0900

    move dataTemplates binding functions to Control.fs

--- src/Avalonia.FuncUI/DSL/Base/Control.fs
+++ src/Avalonia.FuncUI/DSL/Base/Control.fs
@@ -4,7 +4,9 @@ namespace Avalonia.FuncUI.DSL
 module Control =
     open Avalonia.Controls
     open Avalonia.Controls.Primitives
+    open Avalonia.Controls.Templates
     open Avalonia.Interactivity
+    open Avalonia.FuncUI
     open Avalonia.FuncUI.Types
     open Avalonia.FuncUI.Builder
 
@@ -18,6 +20,23 @@ module Control =
 
         static member tag<'t when 't :> Control>(value: obj) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<obj>(Control.TagProperty, value, ValueNone)
+
+        static member dataTemplates<'t when 't :> Control>(templates: DataTemplates) : IAttr<'t> =
+            let name = nameof Unchecked.defaultof<'t>.DataTemplates
+            let getter: 't -> DataTemplates = (fun control -> control.DataTemplates)
+            let setter: ('t * DataTemplates -> unit) = (fun (control, value) -> Setters.avaloniaList control.DataTemplates value)
+            let compare: obj * obj -> bool = EqualityComparers.compareSeq<DataTemplates,_>
+            let factory = fun () -> DataTemplates()
+
+            AttrBuilder<'t>.CreateProperty<DataTemplates>(name, templates, ValueSome getter, ValueSome setter, ValueSome compare, factory)
+
+        static member dataTemplates<'t when 't :> Control>(templates: IDataTemplate list) : IAttr<'t> =
+            let name = nameof Unchecked.defaultof<'t>.DataTemplates
+            let getter: 't -> IDataTemplate list = (fun control -> control.DataTemplates |> Seq.toList)
+            let setter: ('t * IDataTemplate list -> unit) = (fun (control, value) -> Setters.avaloniaList control.DataTemplates value)
+            let factory = fun () -> []
+
+            AttrBuilder<'t>.CreateProperty<IDataTemplate list>(name, templates, ValueSome getter, ValueSome setter, ValueNone, factory)
 
         static member contextMenu<'t when 't :> Control>(menuView: IView<ContextMenu> option) : IAttr<'t> =
             let view =
