18902b5ea87323612412002afdf404a1ac8517da
SilkyFowl <16532218+SilkyFowl@users.noreply.github.com>
2024-05-08 16:52:47 +0900

    add StackPanel bindings.

--- src/Avalonia.FuncUI/DSL/Panels/StackPanel.fs
+++ src/Avalonia.FuncUI/DSL/Panels/StackPanel.fs
@@ -3,20 +3,33 @@ namespace Avalonia.FuncUI.DSL
 [<AutoOpen>]
 module StackPanel =
     open Avalonia.Controls
+    open Avalonia.Interactivity
     open Avalonia.Layout
     open Avalonia.FuncUI.Types
     open Avalonia.FuncUI.Builder
-   
+
     let create (attrs: IAttr<StackPanel> list): IView<StackPanel> =
         ViewBuilder.Create<StackPanel>(attrs)
 
     type StackPanel with
         /// <summary>
+        /// Occurs when the measurements for horizontal snap points change.
+        /// </summary>
+        static member onHorizontalSnapPointsChanged<'t when 't :> StackPanel>(func: RoutedEventArgs -> unit, ?subPatchOptions) : IAttr<'t> =
+            AttrBuilder<'t>.CreateSubscription<RoutedEventArgs>(StackPanel.HorizontalSnapPointsChangedEvent, func, ?subPatchOptions = subPatchOptions)
+
+        /// <summary>
+        /// Occurs when the measurements for vertical snap points change.
+        /// </summary>
+        static member onVerticalSnapPointsChanged<'t when 't :> StackPanel>(func: RoutedEventArgs -> unit, ?subPatchOptions) : IAttr<'t> =
+            AttrBuilder<'t>.CreateSubscription<RoutedEventArgs>(StackPanel.VerticalSnapPointsChangedEvent, func, ?subPatchOptions = subPatchOptions)
+
+        /// <summary>
         /// Gets or sets the size of the spacing to place between child controls.
         /// </summary>
         static member spacing<'t when 't :> StackPanel>(value: double) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<double>(StackPanel.SpacingProperty, value, ValueNone)
-        
+
         /// <summary>
         /// Gets or sets the orientation in which child controls will be layed out.
         /// </summary>
