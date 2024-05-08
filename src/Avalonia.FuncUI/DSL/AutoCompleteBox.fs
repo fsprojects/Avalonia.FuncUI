b577366672199b51699fea6d4a94d5b3d338b960
SilkyFowl <16532218+SilkyFowl@users.noreply.github.com>
2024-05-08 23:06:23 +0900

    add AutoCompleteBox bindings.

--- src/Avalonia.FuncUI/DSL/AutoCompleteBox.fs
+++ src/Avalonia.FuncUI/DSL/AutoCompleteBox.fs
@@ -12,6 +12,8 @@ module AutoCompleteBox =
     open Avalonia.Controls.Templates
     open Avalonia.FuncUI.Types
     open Avalonia.FuncUI.Builder
+    open System.ComponentModel
+    open Avalonia.Data
 
     let create (attrs: IAttr<AutoCompleteBox> list): IView<AutoCompleteBox> =
         ViewBuilder.Create<AutoCompleteBox>(attrs)
@@ -44,6 +46,13 @@ module AutoCompleteBox =
         static member isDropDownOpen<'t when 't :> AutoCompleteBox>(value: bool) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<bool>(AutoCompleteBox.IsDropDownOpenProperty, value, ValueNone)
 
+        static member valueMemberBinding<'t when 't :> AutoCompleteBox>(binding: IBinding) : IAttr<'t> =
+            let name = nameof Unchecked.defaultof<'t>.ValueMemberBinding
+            let getter: 't -> IBinding = (fun control -> control.ValueMemberBinding)
+            let setter: 't * IBinding -> unit = (fun (control, value) -> control.ValueMemberBinding <- value)
+
+            AttrBuilder<'t>.CreateProperty<IBinding>(name, binding, ValueSome getter, ValueSome setter, ValueNone)
+
         static member selectedItem<'t when 't :> AutoCompleteBox>(value: obj) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<obj>(AutoCompleteBox.SelectedItemProperty, value, ValueNone)
 
@@ -56,8 +65,86 @@ module AutoCompleteBox =
         static member onTextChanged<'t when 't :> AutoCompleteBox>(func: string -> unit, ?subPatchOptions) =
             AttrBuilder<'t>.CreateSubscription<string>(AutoCompleteBox.TextProperty, func, ?subPatchOptions = subPatchOptions)
 
+        static member onPopulating<'t when 't :> AutoCompleteBox>(func: PopulatingEventArgs -> unit, ?subPatchOptions) =
+            let name = nameof Unchecked.defaultof<'t>.Populating
+            let factory: SubscriptionFactory<PopulatingEventArgs> =
+                fun (control, func, token) ->
+                    let control = control :?> 't
+                    let handler = EventHandler<PopulatingEventArgs>(fun sender args -> func args)
+                    let event = control.Populating
+
+                    event.AddHandler(handler)
+                    token.Register(fun () -> event.RemoveHandler(handler)) |> ignore
+
+            AttrBuilder<'t>.CreateSubscription<PopulatingEventArgs>(name, factory, func, ?subPatchOptions = subPatchOptions)
+
+        static member onPopulated<'t when 't :> AutoCompleteBox>(func: PopulatedEventArgs -> unit, ?subPatchOptions) =
+            let name = nameof Unchecked.defaultof<'t>.Populated
+            let factory: SubscriptionFactory<PopulatedEventArgs> =
+                fun (control, func, token) ->
+                    let control = control :?> 't
+                    let handler = EventHandler<PopulatedEventArgs>(fun sender args -> func args)
+                    let event = control.Populated
+
+                    event.AddHandler(handler)
+                    token.Register(fun () -> event.RemoveHandler(handler)) |> ignore
+
+            AttrBuilder<'t>.CreateSubscription<PopulatedEventArgs>(name, factory, func, ?subPatchOptions = subPatchOptions)
+
         static member onSearchTextChanged<'t when 't :> AutoCompleteBox>(func: string -> unit, ?subPatchOptions) =
             AttrBuilder<'t>.CreateSubscription<string>(AutoCompleteBox.SearchTextProperty, func, ?subPatchOptions = subPatchOptions)
+
+        static member onDropDownOpening<'t when 't :> AutoCompleteBox>(func: 't * CancelEventArgs -> unit, ?subPatchOptions) =
+            let name = nameof Unchecked.defaultof<'t>.DropDownOpening
+            let factory: SubscriptionFactory<'t * CancelEventArgs> =
+                fun (control, func, token) ->
+                    let control = control :?> 't
+                    let handler = EventHandler<CancelEventArgs>(fun sender args -> func (sender :?> 't, args))
+                    let event = control.DropDownOpening
+
+                    event.AddHandler(handler)
+                    token.Register(fun () -> event.RemoveHandler(handler)) |> ignore
+
+            AttrBuilder<'t>.CreateSubscription<'t * CancelEventArgs>(name, factory, func, ?subPatchOptions = subPatchOptions)
+
+        static member onDropDownOpened<'t when 't :> AutoCompleteBox>(func: 't -> unit, ?subPatchOptions) =
+            let name = nameof Unchecked.defaultof<'t>.DropDownOpened
+            let factory: SubscriptionFactory<'t> =
+                fun (control, func, token) ->
+                    let control = control :?> 't
+                    let handler = EventHandler(fun sender args -> func (sender :?> 't))
+                    let event = control.DropDownOpened
+
+                    event.AddHandler(handler)
+                    token.Register(fun () -> event.RemoveHandler(handler)) |> ignore
+
+            AttrBuilder<'t>.CreateSubscription<'t>(name, factory, func, ?subPatchOptions = subPatchOptions)
+
+        static member onDropDownClosing<'t when 't :> AutoCompleteBox>(func: 't * CancelEventArgs -> unit, ?subPatchOptions) =
+            let name = nameof Unchecked.defaultof<'t>.DropDownClosing
+            let factory: SubscriptionFactory<'t * CancelEventArgs> =
+                fun (control, func, token) ->
+                    let control = control :?> 't
+                    let handler = EventHandler<CancelEventArgs>(fun sender args -> func (sender :?> 't, args))
+                    let event = control.DropDownClosing
+
+                    event.AddHandler(handler)
+                    token.Register(fun () -> event.RemoveHandler(handler)) |> ignore
+
+            AttrBuilder<'t>.CreateSubscription<'t * CancelEventArgs>(name, factory, func, ?subPatchOptions = subPatchOptions)
+
+        static member onDropDownClosed<'t when 't :> AutoCompleteBox>(func: 't -> unit, ?subPatchOptions) =
+            let name = nameof Unchecked.defaultof<'t>.DropDownClosed
+            let factory: SubscriptionFactory<'t> =
+                fun (control, func, token) ->
+                    let control = control :?> 't
+                    let handler = EventHandler(fun sender args -> func (sender :?> 't))
+                    let event = control.DropDownClosed
+
+                    event.AddHandler(handler)
+                    token.Register(fun () -> event.RemoveHandler(handler)) |> ignore
+
+            AttrBuilder<'t>.CreateSubscription<'t>(name, factory, func, ?subPatchOptions = subPatchOptions)
 
         static member filterMode<'t when 't :> AutoCompleteBox>(mode: AutoCompleteFilterMode) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<AutoCompleteFilterMode>(AutoCompleteBox.FilterModeProperty, mode, ValueNone)
@@ -71,6 +158,9 @@ module AutoCompleteBox =
 
         static member textFilter<'t when 't :> AutoCompleteBox>(filterFunc: string -> obj -> bool) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<_>(AutoCompleteBox.TextFilterProperty, filterFunc, ValueNone)
+
+        static member textSelector<'t when 't :> AutoCompleteBox>(selector: AutoCompleteSelector<string>) : IAttr<'t> =
+            AttrBuilder<'t>.CreateProperty<_>(AutoCompleteBox.TextSelectorProperty, selector, ValueNone)
 
         static member dataItems<'t when 't :> AutoCompleteBox>(items: IEnumerable) : IAttr<'t> =
             AttrBuilder<'t>.CreateProperty<IEnumerable>(AutoCompleteBox.ItemsSourceProperty, items, ValueNone)
