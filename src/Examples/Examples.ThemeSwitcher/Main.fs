190afd5a17cbcfd9f7ab3cf0acb15f8f28bf970a
SilkyFowl <16532218+SilkyFowl@users.noreply.github.com>
2025-11-21 08:01:37 +0900

    [wip] Create ResourceDictionary Sample Project.

--- src/Examples/Examples.ThemeSwitcher/Main.fs
+++ src/Examples/Examples.ThemeSwitcher/Main.fs
@@ -10,6 +10,8 @@ module Main =
     open Avalonia.Styling
     open Examples.ThemeSwitcher.DSL
     open Avalonia.Data
+    open Avalonia.Layout
+    open Avalonia.Controls.Presenters
 
 
     module ThemeVariant =
@@ -65,8 +67,9 @@ module Main =
             ResourceDictionary.keyValue ("LargeFontSize", 24.0)
             ResourceDictionary.keyValue ("MediumFontSize", 16.0)
             ResourceDictionary.keyValue ("SmallFontSize", 12.0)
-            ResourceDictionary.keyValue ("DefaultPadding", Thickness (16.0))
-            ResourceDictionary.keyValue ("ButtonPadding", Thickness (12.0, 8.0))
+            ResourceDictionary.keyValue ("DefaultMargin", Thickness 8.0)
+            ResourceDictionary.keyValue ("DefaultPadding", Thickness (12.0))
+            ResourceDictionary.keyValue ("ButtonPadding", Thickness (8.0, 4.0))
 
             // Theme variant-specific resources
             for colorSet in [ ColorSet.dark; ColorSet.light; state.CustomColors ] do
@@ -92,6 +95,9 @@ module Main =
         Seq.iter collection.Add vs
         x
 
+    let inline getResourceBinding<'t when 't :> IResourceHost> (x: 't) (key: string) =
+        x.GetResourceObservable(key).ToBinding ()
+
     let themeVariantScopeView (themeVariant: ThemeVariant) (attrs) =
         ThemeVariantScope.create [
             yield! attrs
@@ -99,63 +105,165 @@ module Main =
             ThemeVariantScope.child (
                 DockPanel.create [
                     DockPanel.init (fun dp ->
+                        let getResourceBinding = getResourceBinding dp
+
                         dp.Styles.AddRange [
+                            Style (fun x -> x.Is<Layoutable> ())
+                            |> addProp _.Setters [
+                                Setter (Layoutable.MarginProperty, getResourceBinding "DefaultMargin")
+                            ]
                             Style (fun x -> x.Is<Panel> ())
                             |> addProp _.Setters [
-                                Setter (
-                                    Panel.BackgroundProperty,
-                                    dp.GetResourceObservable("SurfaceBrush").ToBinding ()
-                                )
+                                Setter (Panel.BackgroundProperty, getResourceBinding "SurfaceBrush")
                             ]
                             Style (fun x -> x.Is<TextBlock> ())
                             |> addProp _.Setters [
-                                Setter (
-                                    TextBlock.ForegroundProperty,
-                                    dp.GetResourceObservable("TextBrush").ToBinding ()
-                                )
-                                Setter (
-                                    TextBlock.PaddingProperty,
-                                    dp.GetResourceObservable("DefaultPadding").ToBinding ()
-                                )
+                                Setter (TextBlock.ForegroundProperty, getResourceBinding "TextBrush")
                             ]
                             Style (fun x -> x.Is<Button> ())
                             |> addProp _.Setters [
-                                Setter (
-                                    Button.BackgroundProperty,
-                                    dp.GetResourceObservable("PrimaryBrush").ToBinding ()
-                                )
-                                Setter (Button.ForegroundProperty, dp.GetResourceObservable("TextBrush").ToBinding ())
-                                Setter (Button.PaddingProperty, dp.GetResourceObservable("ButtonPadding").ToBinding ())
+                                Setter (Button.BackgroundProperty, getResourceBinding "PrimaryBrush")
+                                Setter (Button.ForegroundProperty, getResourceBinding "TextBrush")
+                                Setter (Button.PaddingProperty, getResourceBinding "ButtonPadding")
                             ]
                             |> addProp _.Children [
-                                Style (fun x -> x.Nesting().Class (":pointerover"))
+                                Style (fun x -> x.Nesting().Class ("secondary"))
                                 |> addProp _.Setters [
-                                    Setter (
-                                        TextBlock.BackgroundProperty,
-                                        dp.GetResourceObservable("AccentBrush").ToBinding ()
-                                    )
-                                    Setter (
-                                        TextBlock.ForegroundProperty,
-                                        dp.GetResourceObservable("TextBrush").ToBinding ()
-                                    )
+                                    Setter (Button.BackgroundProperty, getResourceBinding "SecondaryBrush")
+                                    Setter (Button.ForegroundProperty, getResourceBinding "TextBrush")
+                                ]
+                                // "^:pointerover /template/ ContentPresenter#PART_ContentPresenter"
+                                Style (fun x ->
+                                    x
+                                        .Nesting()
+                                        .Class(":pointerover")
+                                        .Template()
+                                        .OfType<ContentPresenter>()
+                                        .Name ("PART_ContentPresenter"))
+                                |> addProp _.Setters [
+                                    Setter (Button.BackgroundProperty, getResourceBinding "AccentBrush")
+                                    Setter (Button.ForegroundProperty, getResourceBinding "TextBrush")
                                 ]
                             ]
                         ])
                     DockPanel.lastChildFill false
                     DockPanel.children [
                         TextBlock.create [
+                            TextBlock.dock Dock.Top
                             TextBlock.text (sprintf "Current Theme: %A" themeVariant)
-                            TextBlock.margin (Thickness (0.0, 0.0, 0.0, 16.0))
-                            TextBlock.fontSize 18.0
+                            TextBlock.classes [ "large" ]
                         ]
-                        Button.create [ Button.content "Sample Button" ]
+                        TextBlock.create [ TextBlock.dock Dock.Top; TextBlock.text "This is a sample text block." ]
+                        StackPanel.create [
+                            StackPanel.dock Dock.Top
+                            StackPanel.orientation Orientation.Horizontal
+                            StackPanel.children [
+                                Button.create [ Button.content "Sample Button" ]
+                                Button.create [ Button.classes [ "secondary" ]; Button.content "Sample Button" ]
+                            ]
+                        ]
                     ]
                 ]
             )
         ]
 
-    let colorPickerView (label: string) (color: Color) =
-        ColorPicker.create [ ColorPicker.color color ]
+    let colorPickerView (color: Color) onColorChanged attrs =
+        ColorPicker.create [
+            yield! attrs
+            ColorPicker.color color
+            ColorPicker.onPropertyChanged (fun e ->
+                if e.Property = ColorPicker.ColorProperty then
+                    let newColor = e.GetNewValue<Color> ()
+                    onColorChanged newColor)
+        ]
+
+    let colorView (label: string) (color: Color) onColorChanged attrs =
+        StackPanel.create [
+            yield! attrs
+            StackPanel.orientation Orientation.Horizontal
+            StackPanel.children [
+                TextBlock.create [ TextBlock.width 120.0; TextBlock.text label; TextBlock.classes [ "small" ] ]
+                colorPickerView color onColorChanged []
+            ]
+        ]
+
+    let colorSetsView (state: IWritable<State>) (attrs) =
+        Component.create (
+            "colorSetsView",
+            fun ctx ->
+                let state = ctx.usePassedLazy (fun _ -> state)
+                let defaultMargin = ctx.useStateLazy (fun () -> Thickness 0.0)
+
+                ctx.attrs [
+                    Component.onResourceObservable (
+                        "DefaultMargin",
+                        function
+                        | Some (:? Thickness as v) -> defaultMargin.Set v
+                        | _ -> ()
+                    )
+                    yield! attrs
+                ]
+
+                DockPanel.create [
+                    DockPanel.lastChildFill false
+                    DockPanel.classes [ "colorsetsview" ]
+                    DockPanel.children [
+                        TextBlock.create [
+                            TextBlock.dock Dock.Top
+                            TextBlock.classes [ "large" ]
+                            TextBlock.text $"Color Sets View: {state.Current.CustomColors.Theme}"
+                        ]
+                        colorView
+                            "Primary Color"
+                            state.Current.CustomColors.Primary
+                            (fun newColor ->
+                                state.Set
+                                    { state.Current with
+                                        CustomColors.Primary = newColor })
+                            [ DockPanel.dock Dock.Top; StackPanel.margin defaultMargin.Current ]
+                        colorView
+                            "Secondary Color"
+                            state.Current.CustomColors.Secondary
+                            (fun newColor ->
+                                state.Set
+                                    { state.Current with
+                                        CustomColors.Secondary = newColor })
+                            [ DockPanel.dock Dock.Top; StackPanel.margin defaultMargin.Current ]
+                        colorView
+                            "Background Color"
+                            state.Current.CustomColors.Background
+                            (fun newColor ->
+                                state.Set
+                                    { state.Current with
+                                        CustomColors.Background = newColor })
+                            [ DockPanel.dock Dock.Top; StackPanel.margin defaultMargin.Current ]
+                        colorView
+                            "Surface Color"
+                            state.Current.CustomColors.Surface
+                            (fun newColor ->
+                                state.Set
+                                    { state.Current with
+                                        CustomColors.Surface = newColor })
+                            [ DockPanel.dock Dock.Top; StackPanel.margin defaultMargin.Current ]
+                        colorView
+                            "Text Color"
+                            state.Current.CustomColors.Text
+                            (fun newColor ->
+                                state.Set
+                                    { state.Current with
+                                        CustomColors.Text = newColor })
+                            [ DockPanel.dock Dock.Top; StackPanel.margin defaultMargin.Current ]
+                        colorView
+                            "Accent Color"
+                            state.Current.CustomColors.Accent
+                            (fun newColor ->
+                                state.Set
+                                    { state.Current with
+                                        CustomColors.Accent = newColor })
+                            [ DockPanel.dock Dock.Top; StackPanel.margin defaultMargin.Current ]
+                    ]
+                ]
+        )
 
     let view () =
         Component (fun ctx ->
@@ -163,9 +271,30 @@ module Main =
             ctx.attrs [ Component.resources (createThemeResources state.Current) ]
 
             Grid.create [
+                Grid.init (fun grid ->
+                    let getResourceBinding = getResourceBinding grid
+
+                    grid.Styles.AddRange [
+                        Style (fun x -> x.Is<TextBlock> ())
+                        |> addProp _.Setters [
+                            Setter (TextBlock.FontSizeProperty, getResourceBinding "MediumFontSize")
+                            Setter (TextBlock.PaddingProperty, getResourceBinding "ButtonPadding")
+                        ]
+                        |> addProp _.Children [
+                            Style (fun x -> x.Nesting().Class ("large"))
+                            |> addProp _.Setters [
+                                Setter (TextBlock.FontSizeProperty, getResourceBinding "LargeFontSize")
+                            ]
+                            Style (fun x -> x.Nesting().Class ("small"))
+                            |> addProp _.Setters [
+                                Setter (TextBlock.FontSizeProperty, getResourceBinding "SmallFontSize")
+                            ]
+                        ]
+                    ])
                 Grid.rowDefinitions "Auto,Auto,Auto"
-                Grid.columnDefinitions "*"
+                Grid.columnDefinitions "*,Auto"
                 Grid.children [
+                    colorSetsView state [ Grid.row 0; Grid.column 1; Grid.rowSpan 3 ]
                     themeVariantScopeView ThemeVariant.Dark [ Grid.row 0; Grid.column 0 ]
                     themeVariantScopeView ThemeVariant.Light [ Grid.row 1; Grid.column 0 ]
                     themeVariantScopeView ThemeVariant.Custom [ Grid.row 2; Grid.column 0 ]
--- src/Examples/Examples.ThemeSwitcher/Program.fs
+++ src/Examples/Examples.ThemeSwitcher/Program.fs
@@ -6,16 +6,21 @@ open Avalonia.Controls.ApplicationLifetimes
 open Avalonia.Themes.Fluent
 open Avalonia.FuncUI
 open Avalonia.FuncUI.Hosts
-open Avalonia.Markup.Xaml.Styling
 
 type MainWindow() =
     inherit HostWindow()
 
     do
         base.Title <- "Theme Switcher Example"
+
+        base.Icon <-
+            System.Uri "avares://Examples.ThemeSwitcher/Assets/Icons/icon.ico"
+            |> Platform.AssetLoader.Open
+            |> WindowIcon
+
         base.Width <- 800.0
-        base.Height <- 600.0
         base.MinWidth <- 600.0
+        base.Height <- 680.0
         base.MinHeight <- 400.0
         base.Content <- Main.view ()
 
@@ -23,18 +28,18 @@ type App() =
     inherit Application()
 
     override this.Initialize() =
-        this.Styles.Add(FluentTheme())
+        this.Styles.Add (FluentTheme ())
         this.Styles.Load "avares://Avalonia.Controls.ColorPicker/Themes/Fluent/Fluent.xaml"
         this.RequestedThemeVariant <- Styling.ThemeVariant.Dark
 
 #if DEBUG
-        this.AttachDeveloperTools() |> ignore
+        this.AttachDeveloperTools () |> ignore
 #endif
 
     override this.OnFrameworkInitializationCompleted() =
         match this.ApplicationLifetime with
         | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
-            let mainWindow = MainWindow()
+            let mainWindow = MainWindow ()
             desktopLifetime.MainWindow <- mainWindow
         | _ -> ()
 
@@ -42,4 +47,4 @@ module Program =
 
     [<EntryPoint>]
     let main (args: string[]) =
-        AppBuilder.Configure<App>().UsePlatformDetect().UseSkia().StartWithClassicDesktopLifetime(args)
+        AppBuilder.Configure<App>().UsePlatformDetect().UseSkia().StartWithClassicDesktopLifetime (args)
