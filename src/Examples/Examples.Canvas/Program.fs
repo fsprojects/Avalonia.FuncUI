ab0f04f0247a9ef4d7eb9a3d83cfaada1db22fe3
Seres Lóránt <sereslorant@gmail.com>
2026-04-07 13:50:37 +0200

    Adding canvas example.

---
+++ src/Examples/Examples.Canvas/Program.fs
@@ -0,0 +1,36 @@
+﻿namespace Examples.Canvas
+
+open Avalonia
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.Themes.Fluent
+open Avalonia.FuncUI.Hosts
+
+type MainWindow() =
+    inherit HostWindow()
+    do
+        base.Title <- "Canvas"
+        base.Height <- 462.0
+        base.Width <- 550.0
+        base.Content <- View.mainView()
+
+type App() =
+    inherit Application()
+
+    override this.Initialize() =
+        this.Styles.Add(FluentTheme())
+        this.RequestedThemeVariant <- Styling.ThemeVariant.Dark
+
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
+            desktopLifetime.MainWindow <- MainWindow()
+        | _ -> ()
+
+module Program =
+    [<EntryPoint>]
+    let main (args: string[]) =
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .UseSkia()
+            .StartWithClassicDesktopLifetime(args)
